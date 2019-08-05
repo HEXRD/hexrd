8eb6c77013f98bb504de7825f2abf8305fdbf2f2
Joel Bernier <joelvbernier@me.com>
2019-08-05 16:03:16 -0700

    added some plotting

--- scripts/calibration_mockup.py
+++ scripts/calibration_mockup.py
@@ -440,3 +440,35 @@ pc = PowderCalibrator(instr, plane_data, img_dict,
 ic = InstrumentCalibrator(pc)
 
 ic.run_calibration()
+
+# %% sample plot to check fit line poistions ahead of fitting
+fig, ax = plt.subplots(2, 2)
+fig_row, fig_col = np.unravel_index(np.arange(instr.num_panels), (2, 2))
+
+data_dict = pc._extract_powder_lines()
+
+ifig = 0
+for det_key, panel in instr.detectors.items():
+    all_pts = np.vstack(data_dict[det_key])
+    '''
+    pimg = equalize_adapthist(
+            rescale_intensity(img_dict[det_key], out_range=(0, 1)),
+            10, clip_limit=0.2)
+    '''
+    pimg = np.array(img_dict[det_key], dtype=float)
+    pimg[~panel.panel_buffer] = np.nan
+    ax[fig_row[ifig], fig_col[ifig]].imshow(
+        pimg,
+        vmin=np.percentile(img_dict[det_key], 5),
+        vmax=np.percentile(img_dict[det_key], 90),
+        cmap=plt.cm.bone_r
+    )
+    ideal_angs, ideal_xys = panel.make_powder_rings(plane_data,
+                                                    delta_eta=eta_tol)
+    rijs = panel.cartToPixel(np.vstack(ideal_xys))
+    ax[fig_row[ifig], fig_col[ifig]].plot(rijs[:, 1], rijs[:, 0], 'cx')
+    ax[fig_row[ifig], fig_col[ifig]].set_title(det_key)
+    rijs = panel.cartToPixel(all_pts[:, :2])
+    ax[fig_row[ifig], fig_col[ifig]].plot(rijs[:, 1], rijs[:, 0], 'm+')
+    ax[fig_row[ifig], fig_col[ifig]].set_title(det_key)
+    ifig += 1
