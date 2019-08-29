650a9132e9f78193de50ebae49b57e253881c0ed
Chris Harris <chris.harris@kitware.com>
2019-08-29 12:22:42 -0400

    Add conda recipe

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,3 @@
+rmdir build /s /q
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+if errorlevel 1 exit 1
---
+++ conda.recipe/build.sh
@@ -0,0 +1,3 @@
+rm -rf build
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
