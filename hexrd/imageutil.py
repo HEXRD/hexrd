9b6120b4838530bf1bd4ff2cacd1a8211f437757
Joel Bernier <joelvbernier@me.com>
2019-10-31 17:56:19 -0700

    added astropy-based SNIP

--- hexrd/imageutil.py
+++ hexrd/imageutil.py
@@ -1,35 +1,43 @@
 import numpy as np
 from scipy import ndimage
 
-
-'''
 from astropy import convolution
 
-def snip1d(y, w=4, numiter=3):
+
+def snip1d(y, w=4, numiter=3, threshold=0):
     """
     Return SNIP-estimated baseline-background for given spectrum y.
+
+    !!!: threshold values get marked as NaN in convolution
     """
+    mask = y <= threshold
     zfull = np.log(np.log(np.sqrt(y + 1) + 1) + 1)
     bkg = np.zeros_like(zfull)
     for k, z in enumerate(zfull):
-        b = z
-        for i in range(numiter):
-            for p in range(w, 0, -1):
-                kernel = np.zeros(p*2 + 1)
-                kernel[0] = kernel[-1] = 1./2.
-                b = np.minimum(
-                    b,
-                    convolution.convolve(z, kernel, boundary='extend')
-                )
-            z = b
-        bkg[k, :] = (np.exp(np.exp(b) - 1) - 1)**2 - 1
+        if np.all(mask[k]):
+            bkg[k, :] = np.nan
+        else:
+            b = z
+            for i in range(numiter):
+                for p in range(w, 0, -1):
+                    kernel = np.zeros(p*2 + 1)
+                    kernel[0] = kernel[-1] = 1./2.
+                    b = np.minimum(
+                        b,
+                        convolution.convolve(z, kernel, boundary='extend', mask=mask[k])
+                    )
+                z = b
+            bkg[k, :] = (np.exp(np.exp(b) - 1) - 1)**2 - 1
+    nan_idx = np.isnan(bkg)
+    bkg[nan_idx] = threshold
     return bkg
-'''
 
 
-def snip1d(y, w=4, numiter=2, mask=None):
+def fast_snip1d(y, w=4, numiter=2, mask=None):
     """
     Return SNIP-estimated baseline-background for given spectrum y.
+
+    FIXME: behavior of mask kwarg is not finalized
     """
     if mask is not None:
         for ir, rmask in enumerate(mask):
