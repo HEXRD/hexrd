e95508bb74a84c216886c2767d1294166b8ffccd
Saransh <saransh1@llnl.gov>
2020-01-16 12:58:47 -0800

    material class only calculates structure factor for allowed hkls by default. reitveld class has major changes with implementation of sequential linear optimization, structure factors, flexible refinement strategy using dictionaries etc.

--- hexrd/Rietveld.py
+++ hexrd/Rietveld.py
@@ -1,8 +1,7 @@
 import numpy as np
-from hexrd import material
 import warnings
 from hexrd.imageutil import snip1d
-from scipy.optimize import minimize, Bounds
+from scipy.optimize import minimize, Bounds, shgo
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
@@ -26,29 +25,46 @@ class Rietveld:
 	def __init__(self, pdata, inp_spectrum, 
 				 spec_tth_min, spec_tth_max,
 				 bkgmethod='spline', snipw=8, 
-				 numiter=2, deg=8):
+				 numiter=2, deg=8, parms_to_refine={}):
 
 		'''
 			these contain the main refinement parameters for the rietveld code
+			if an empty dictionary is passed, then all refine options
+			are set to true. otherwise the options to refine are set
+			according to the passed values
+
+			each key in the dictionary corresponds to the following list
+			parms_to_refine[key] = [True/False, initial guess, lower bound, upper bound]
 		'''
 
 		# plane data
 		self._pdata = pdata
 
 		# Cagliotti parameters for the half width of the peak
-		self._U = 1.0e-4
-		self._V = 1.0e-4
-		self._W = 1.0e-4
+		self._U = 1.0e-3
+		self._V = 1.0e-3
+		self._W = 1.0e-3
 
 		# Pseudo-Voight mixing parameters
 		self._eta1 = 4.0e-1
 		self._eta2 = 1.0e-2
 		self._eta3 = 1.0e-2
 
-		# peak intensity
-		sf 			= pdata.get_structFact()
-		sf 			= sf / np.amax(sf)
-		self._Imax 	= 1.0
+		''' maximum intensity, Imax = S * Mk * Lk * |Fk|**2 * Pk * Ak * Ek * ...
+			S  = arbitrary scale factor
+			Mk = multiplicity of reflection
+			Lk = lorentz polarization factor
+			Fk = structure factor
+			Pk = preferrred orientation 
+			Ak = absorption correction
+			Ek = extinction correction 
+			...
+			...
+			etc.
+		'''
+		self.initialize_Imax()
+
+		self.parms_to_refine = parms_to_refine
 
 		# initialize the spectrum class
 		self.Spectrum  = self.Spectrum(inp_spectrum, spec_tth_min, spec_tth_max,
@@ -58,14 +74,10 @@ class Rietveld:
 		self.generate_tthlist()
 
 		# initialize simulated spectrum
-		self.initialize_spectrum()
-
-		x0 		= np.asarray([self.U,self.V,self.W,self.eta1,self.eta2,self.eta3])
-		bounds 	= Bounds([0.0, 0.0, 0.0, 0.0, 0.0, 0.0], \
-						 [1e-2, 1e-2, 1e-2, 1.0, 1.0, 1.0]) 
+		self.initialize_spectrum()  
 
 		# initialize the refine class
-		self.Optimize = self.Refine(x0, bounds)
+		self.Optimize = self.Refine(self.refine_dict)
 
 	def checkangle(ang, name):
 
@@ -89,11 +101,11 @@ class Rietveld:
 
 	def Gaussian(self, tth):
 		beta = 0.5 * self.H * np.sqrt(np.pi / np.log(2.0))
-		self.GaussianI = self.Imax * np.exp(-np.pi * (self.tth_list - tth)**2 / beta**2)
+		self.GaussianI = np.exp(-np.pi * (self.tth_list - tth)**2 / beta**2)
 
 	def Lorentzian(self, tth):
 		w = 0.5 * self.H
-		self.LorentzI = self.Imax * (w**2 / (w**2 + (self.tth_list - tth)**2 ) )
+		self.LorentzI = (w**2 / (w**2 + (self.tth_list - tth)**2 ) )
 
 	def PseudoVoight(self, tth):
 		self.Gaussian(tth)
@@ -103,6 +115,15 @@ class Rietveld:
 	def SpecimenBroadening(self):
 		self.SB = np.sinc((self.tth_list - self.tth) / 2.0 / np.pi)
 
+	def initialize_sf(self):
+		sf 			= self.pdata.get_structFact()
+		self.sf 	= sf / np.amax(sf)
+
+	def initialize_Imax(self):
+
+		self.initialize_sf()
+		self._Imax = self.sf
+
 	def initialize_spectrum(self):
 
 		peak_tth 		= self.pdata.getTTh()
@@ -110,18 +131,22 @@ class Rietveld:
 						  (peak_tth > self.Spectrum.spec_tth_min)
 		peak_tth 		= peak_tth[mask]
 
+		Imax 			= self.Imax[mask]
+
 		if(self.Spectrum.ndim == 1):
 			self.spec_sim 	= np.zeros(self.Spectrum.nspec[0])
 
 		elif(self.Spectrum.ndim == 2):
 			self.spec_sim 	= np.zeros(self.Spectrum.nspec[1])
 
-		for tth in peak_tth:
+		for i in np.arange(peak_tth.shape[0]):
 
+			tth = peak_tth[i]
+			I 	= Imax[i]
 			self.CagliottiH(tth)
 			self.MixingFact(tth)
 			self.PseudoVoight(tth)
-			self.spec_sim += self.PV
+			self.spec_sim += I * self.PV
 
 	def generate_tthlist(self):
 
@@ -149,25 +174,95 @@ class Rietveld:
 	'''
 	def evalResidual(self, x0):
 
-		self.U = x0[0]
-		self.V = x0[1]
-		self.W = x0[2]
+		ctr = 0
+		if(self.refine_dict['cagliotti'][0]):
+			self.U = x0[ctr]
+			self.V = x0[ctr+1]
+			self.W = x0[ctr+2]
+			ctr += 3
 
-		self.eta1 = x0[3]
-		self.eta2 = x0[4]
-		self.eta3 = x0[5]
+		if(self.refine_dict['eta'][0]):
+			self.eta1 = x0[ctr]
+			self.eta2 = x0[ctr+1]
+			self.eta3 = x0[ctr+2]
+			ctr += 3
+
+		if(self.refine_dict['lparms'][0]):
+			pass
+
+		if(self.refine_dict['specimenbroad'][0]):
+			pass
+
+		if(self.refine_dict['strain'][0]):
+			pass
+
+		if(self.refine_dict['texture'][0]):
+			pass
 
 		# return residual as percent
-		residual = 100.0 * np.sum((self.spec_sim - self.Spectrum.spec_arr_nbkg)**2) / \
-						   np.sum(self.Spectrum.spec_arr_nbkg**2)
+		residual = 100.0 * np.sqrt( np.sum((self.spec_sim - self.Spectrum.spec_arr_nbkg)**2) / \
+						   np.sum(self.Spectrum.spec_arr_nbkg**2) )
+
+		# also update initial values in refine class 
+		self.initialize_refinedict()
+
 		return residual
 
+	'''
+		define the various parameters that need to be refined along
+		with their initial values and the lower and upper bounds
+	'''
+	def initialize_refinedict(self):
+
+		refine_dict = {}
+
+		''' 
+			initial values of all the refinable parameters
+		'''
+		x0_cag 		= np.array([self.U, self.V, self.W]) 
+		x0_eta 		= np.array([self.eta1, self.eta2, self.eta3])
+		x0_lparms 	= np.array([])
+		x0_sb 		= np.array([])
+		x0_strain 	= np.array([])
+		x0_tex 		= np.array([])
+
+		'''
+			bounds for all refinable parameters
+		'''
+		bounds_cag 		= Bounds(np.array([0.0, 0.0, 0.0]),np.array([1e-2, 1e-2, 1e-2]))
+		bounds_eta 		= Bounds(np.array([0.0, 0.0, 0.0]),np.array([1e-2, 1e-2, 1e-2]))
+		bounds_lparms 	= Bounds(np.array([]),np.array([]))
+		bounds_sb 		= Bounds(np.array([]),np.array([]))
+		bounds_strain 	= Bounds(np.array([]),np.array([]))
+		bounds_tex 		= Bounds(np.array([]),np.array([]))
+
+
+		if (self.parms_to_refine == {}):
+			refine_dict = { 'cagliotti' : [True, x0_cag, bounds_cag],
+							'eta' : [True, x0_eta, bounds_eta], 
+							'lparms' : [True, x0_lparms, bounds_lparms], 
+							'specimenbroad' : [True, x0_sb, bounds_sb], 
+							'strain' : [True, x0_strain, bounds_strain], 
+							'texture' : [True, x0_tex, bounds_tex] }
+		else:
+			refine_dict = { 'cagliotti' : [False, x0_cag, bounds_cag], 
+							'eta' : [False, x0_eta, bounds_eta], 
+							'lparms' : [False, x0_lparms, bounds_lparms], 
+							'specimenbroad' : [False, x0_sb, bounds_sb], 
+							'strain' : [False, x0_strain, bounds_strain], 
+							'texture' : [False, x0_tex, bounds_tex]
+							}
+			for key, val in self.parms_to_refine.items():
+				refine_dict[key][0] = val
+
+		self.refine_dict = refine_dict
+	
 	def fit(self):
 
-		res = minimize(self.evalResidual, self.Optimize.x0, \
-					   method=self.Optimize.method, \
-					   options=self.Optimize.options, \
-					   bounds = self.Optimize.bounds)
+		res = minimize(	self.evalResidual, self.Optimize.x0, \
+					   	method=self.Optimize.method, \
+					   	options=self.Optimize.options, \
+					   	bounds = self.Optimize.bounds)
 
 		print(res.message+'\t'+'[ Exit status '+str(res.status)+' ]')
 		print('\t minimum function value: '+str(res.fun))
@@ -175,6 +270,8 @@ class Rietveld:
 		print('\t function evaluations: '+str(res.nfev))
 		print('\t gradient evaluations: '+str(res.njev))
 		print('\t optimum values of parameters: '+str(res.x))
+
+		self.initialize_refinedict()
 		return res
 
 	'''
@@ -256,6 +353,29 @@ class Rietveld:
 	def pdata(self, inp_pdata):
 		self._pdata = inp_pdata
 		self.initialize_spectrum()
+		self.initialize_sf()
+		self.initialize_Imax()
+
+	@property
+	def parms_to_refine(self):
+		return self._parms_to_refine
+	
+	@parms_to_refine.setter
+	def parms_to_refine(self,val):
+		assert(type(val) == dict), 'parms_to_refine should be a dictionary'
+		self._parms_to_refine = val
+		self.initialize_refinedict() 
+		self.Optimize = self.Refine(self.refine_dict)
+
+	@property
+	def refine_dict(self):
+		return self._refine_dict
+	
+	@refine_dict.setter
+	def refine_dict(self, val):
+		assert(type(val) == dict), 'parms_to_refine should be a dictionary'
+		self._refine_dict = val
+		self.Optimize = self.Refine(self.refine_dict)
 
 	''' ======================================================================================================== 
 		======================================================================================================== 
@@ -325,6 +445,7 @@ class Rietveld:
 				self.background 	= background
 
 			elif (self.method.lower() == 'spline'):
+
 				'''
 					the cubic spline seems to be the ideal route in terms
 					of determining the background intensity. this involves 
@@ -336,7 +457,6 @@ class Rietveld:
 				self.selectpoints()
 				x = self.points[:,0]
 				y = self.points[:,1]
-
 				self.splinefit(x, y)
 
 			elif (self.method.lower() == 'poly'):
@@ -348,8 +468,6 @@ class Rietveld:
 		def remove_bkg(self):
 
 			self._spec_arr_nbkg = self.spec_arr - self.background
-			mask = self._spec_arr_nbkg < 0.0
-			self._spec_arr_nbkg[mask] = 0.0
 
 		def selectpoints(self):
 
@@ -362,6 +480,7 @@ class Rietveld:
 
 			self.points = np.asarray(plt.ginput(5,timeout=-1, show_clicks=True))			
 			plt.close()
+
 		# define all properties of this class
 		@property
 		def spec_arr(self):
@@ -378,12 +497,6 @@ class Rietveld:
 		@property
 		def spec_arr_nbkg(self):
 			return self._spec_arr_nbkg
-
-		# @spec_arr.setter
-		# def spec_arr_nbkg(self, val):
-		# 	assert(val.ndim == self.ndim), "dimensionality not equal"
-		# 	assert(np.all(val.shape == self.nspec)), " number of dimensions do not match"
-		# 	self._spec_arr_nbkg = val
 
 		''' ndim can't be set, so will have no setter function 
 			ndim tells if the spectrum is 1-d, 2-d etc.
@@ -480,131 +593,6 @@ class Rietveld:
 			assert(np.all(val.shape == self.nspec)), " shape of background does not match background"
 			self._background = val
 			self.remove_bkg()
-	''' ======================================================================================================== 
-		========================================================================================================
-
-	>> @AUTHOR:   	Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
-	>> @DATE:     	01/08/2020 SS 1.0 original
-	>> @DETAILS:  	this is the peak class which characterizes each peak. the overall 
-					spectrum is a collection of such peaks
-					the peak class will be a sub-class of the main Rietveld class
-
-		======================================================================================================== 
-		========================================================================================================
-	'''
-	class Peak:
-
-		def __init__(self, tth):
-
-			Rietveld.checkangle(tth, 'peak two theta')
-
-			# 2theta for the peak
-			self._tth = tth
-
-			# range of angles for which the peak is calculated
-			self._tth_min 	= Rietveld.Spectrum.spec_tth_min
-			self._tth_max 	= Rietveld.Spectrum.spec_tth_max
-
-			if (Rietveld.Spectrum.ndim == 1):
-				self._tth_step 	= (self._tth_max - self._tth_min) / Rietveld.Spectrum.nspec[0]
-
-			elif (Rietveld.Spectrum.ndim == 2):
-				self._tth_step 	= (self._tth_max - self._tth_min) / Rietveld.Spectrum.nspec[1]	
-
-			else:
-				raise ValueError('incorrect number of dimensions in spectrum')
-
-			''' maximum intensity, Imax = S * Mk * Lk * |Fk|**2 * Pk * Ak * Ek * ...
-				S  = arbitrary scale factor
-				Mk = multiplicity of reflection
-				Lk = lorentz polarization factor
-				Fk = structure factor
-				Pk = preferrred orientation 
-				Ak = absorption correction
-				Ek = extinction correction 
-				...
-				...
-				etc.
-			'''
-			self.Imax = 1.0
-
-			'''
-				get all the parameters for the peak 
-			'''
-			self.CagliottiH()
-			self.MixingFact()
-
-			'''
-				get the actual peaks
-			'''
-			self.Gaussian()
-			self.Lorentzian()
-			self.PseudoVoight()
-			self.SpecimenBroadening()
-
-			'''
-				set the parameter for background estimation
-				the 5 times the ratio of halfwidth to step size was figured
-				out empirically
-			'''
-			self.snipw = int(np.round(5.0 * self.H / self.tth_step))
-
-
-		@property
-		def tth(self):
-			return self._tth
-
-		@tth.setter
-		def tth(self, val):
-			self._tth = val
-			self.CagliottiH()
-			self.MixingFact()
-			self.Gaussian()
-			self.Lorentzian()
-			self.PseudoVoight()
-
-			return
-
-		@property
-		def tth_min(self):
-			return self._tth_min
-
-		@tth_min.setter
-		def tth_min(self, val):
-			self._tth_min = val
-			self.Gaussian()
-			self.Lorentzian()
-			self.PseudoVoight()
-			return
-
-		@property
-		def tth_max(self):
-			return self._tth_max
-
-		@tth_max.setter
-		def tth_max(self, val):
-			self._tth_max = val
-			self.Gaussian()
-			self.Lorentzian()
-			self.PseudoVoight()
-			return
-
-		@property
-		def tth_step(self):
-			return self._tth_step
-
-		@tth_step.setter
-		def tth_step(self, val):
-			self._tth_step = val
-			self.Gaussian()
-			self.Lorentzian()
-			self.PseudoVoight()
-			return
-
-		# tth_list is read only
-		@property
-		def tth_list(self):
-			return np.arange(self.tth_min, self.tth_max, self.tth_step)
 
 	''' ======================================================================================================== 
 		========================================================================================================
@@ -623,13 +611,42 @@ class Rietveld:
 		'''
 			set up the minimization class
 		'''
-		def __init__(self,x0,bounds,method='SLSQP',ftol=1e-9,disp=False):
+		def __init__(self, refine_dict, method='SLSQP', ftol=1e-9, disp=False):
 
-			self.bounds 		= bounds
-			self.x0 			= x0
-
+			'''
+				initialize the class
+			'''
 			self.method 		= 'SLSQP'
 			self.ftol 			= 1e-9
 			self.disp 			= False
 			self.options 		= {'ftol':self.ftol, \
 								   'disp':self.disp}
+			self.refine_dict	= refine_dict 						
+
+		def init_vals_bounds(self):
+
+			'''
+				set initial values and bounds for the different refinement parameters
+			'''
+			x0 = []
+			lb = []
+			ub = []
+
+			for key, val in self.refine_dict.items():
+				if(val[0]):
+					x0 = x0 + list(val[1])
+					lb = lb + list(val[2].lb)
+					ub = ub + list(val[2].ub)
+
+			bounds 				= Bounds(lb, ub)
+			self.x0 			= np.array(x0)
+			self.bounds 		= bounds
+
+		@property
+		def refine_dict(self):
+			return self._refine_dict
+		
+		@refine_dict.setter
+		def refine_dict(self, val):
+			self._refine_dict = val
+			self.init_vals_bounds()
--- hexrd/material.py
+++ hexrd/material.py
@@ -158,7 +158,7 @@ class Material(object):
                                              self._beamEnergy.value,
                                              sgsetting=self._sgsetting
                                              )
-        hkls = self.planeData.getHKLs(allHKLs=True)
+        hkls = self.planeData.getHKLs()
         sf = numpy.zeros([hkls.shape[0],])
         for i,g in enumerate(hkls):
             sf[i] = self.unitcell.CalcXRSF(g)
