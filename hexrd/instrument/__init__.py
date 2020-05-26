5990188939d431bbe0801485fc21751f3cd2c5cb
Joel Vincent Bernier <joelvbernier@icloud.com>
2020-05-26 13:23:05 -0700

    removing instrument package; merge from wrong branch

--- hexrd/instrument/__init__.py
+++
@@ -1,7 +0,0 @@
-from .instrument import HEDMInstrument
-from . import beam
-from . import oscillation_stage
-from .detector import PlanarDetector
-from .eta_omega import EtaOmeMap
-from .io import PatchDataWriter, GrainDataWriter, GrainDataWriter_h5
-from .io import unwrap_dict_to_h5
--- hexrd/instrument/tests/common.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import unittest
-
-from hexrd import instrument
-
-class InstrumentTest(unittest.TestCase):
-    pass
