ed0b233a5dc3d923f7e68fcd2fb8b4d4e32120ee
Chris Harris <chris.harris@kitware.com>
2020-06-26 16:10:33 -0400

    Use new convolution function

--- hexrd/imageutil.py
+++ hexrd/imageutil.py
@@ -1,73 +1,38 @@
 import numpy as np
 from scipy import ndimage
 
-try:
-    from astropy import convolution
-    have_astropy = True
-except(ModuleNotFoundError):
-    have_astropy = False
+from hexrd import convolution
 
 
-def fast_snip1d(y, w=4, numiter=2, mask=None):
+def snip1d(y, w=4, numiter=2, threshold=0):
     """
     Return SNIP-estimated baseline-background for given spectrum y.
 
-    FIXME: behavior of mask kwarg is not finalized
+    !!!: threshold values get marked as NaN in convolution
     """
-    if mask is not None:
-        for ir, rmask in enumerate(mask):
-            if np.sum(~rmask) > 0:
-                y[ir, rmask] = np.median(y[ir, ~rmask])
-    z = np.log(np.log(np.sqrt(y + 1) + 1) + 1)
-    b = z
-    for i in range(numiter):
-        for p in range(w, 0, -1):
-            kernel = np.zeros(p*2 + 1)
-            kernel[0] = kernel[-1] = 1./2.
-            b = np.minimum(
-                b,
-                ndimage.convolve1d(z, kernel, mode='reflect')
-            )
-        z = b
-    bkg = (np.exp(np.exp(b) - 1) - 1)**2 - 1
-    return bkg
-
-
-if have_astropy:
-    def snip1d(y, w=4, numiter=2, threshold=0):
-        """
-        Return SNIP-estimated baseline-background for given spectrum y.
-
-        !!!: threshold values get marked as NaN in convolution
-        """
-        mask = y <= threshold
-        zfull = np.log(np.log(np.sqrt(y + 1) + 1) + 1)
-        bkg = np.zeros_like(zfull)
-        for k, z in enumerate(zfull):
-            if np.all(mask[k]):
-                bkg[k, :] = np.nan
-            else:
-                b = z
-                for i in range(numiter):
-                    for p in range(w, 0, -1):
-                        kernel = np.zeros(p*2 + 1)
-                        kernel[0] = kernel[-1] = 1./2.
-                        b = np.minimum(
-                            b,
-                            convolution.convolve(
-                                z, kernel, boundary='extend', mask=mask[k]
-                            )
+    mask = y <= threshold
+    zfull = np.log(np.log(np.sqrt(y + 1) + 1) + 1)
+    bkg = np.zeros_like(zfull)
+    for k, z in enumerate(zfull):
+        if np.all(mask[k]):
+            bkg[k, :] = np.nan
+        else:
+            b = z
+            for i in range(numiter):
+                for p in range(w, 0, -1):
+                    kernel = np.zeros(p*2 + 1)
+                    kernel[0] = kernel[-1] = 1./2.
+                    b = np.minimum(
+                        b,
+                        convolution.convolve(
+                            z, kernel, boundary='extend', mask=mask[k]
                         )
-                    z = b
-                bkg[k, :] = (np.exp(np.exp(b) - 1) - 1)**2 - 1
-        nan_idx = np.isnan(bkg)
-        bkg[nan_idx] = threshold
-        return bkg
-else:
-    def snip1d(y, w=4, numiter=2, threshold=0):
-        mask = y <= threshold
-        return fast_snip1d(y, w=4, numiter=2, mask=mask)
-
+                    )
+                z = b
+            bkg[k, :] = (np.exp(np.exp(b) - 1) - 1)**2 - 1
+    nan_idx = np.isnan(bkg)
+    bkg[nan_idx] = threshold
+    return bkg
 
 def snip1d_quad(y, w=4, numiter=2):
     """Return SNIP-estimated baseline-background for given spectrum y.
