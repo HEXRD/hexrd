bdfe36a968bc3850f272eea7df074a53da6863bc
Chris Harris <chris.harris@kitware.com>
2020-08-14 12:53:06 -0400

    Build Linux package in CentOS 6.11 container
    This ensures that the native libraries are built with a suitability
    old version of glibc so the package will work on older Linux
    distros.

---
+++ .github/workflows/container_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd /github/workspace/
+yum install -y wget git gcc
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod a+x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+${HOME}/miniconda3/bin/conda create -y -n hexrd
+${HOME}/miniconda3/bin/activate hexrd
+${HOME}/miniconda3/bin/conda activate hexrd
+${HOME}/miniconda3/bin/conda install conda-build
+mkdir output
+${HOME}/miniconda3/bin/conda build --output-folder output/ conda.recipe/
