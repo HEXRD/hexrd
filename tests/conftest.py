58f06635e3a96ab4c779002394ef9cb71460f852
Chris Harris <chris.harris@kitware.com>
2020-09-01 13:47:58 -0400

    Add fit grains test

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+import pytest
+
+@pytest.fixture
+def example_repo_path():
+    if 'HEXRD_EXAMPLE_REPO_PATH' not in os.environ:
+        pytest.fail('Environment varable HEXRD_EXAMPLE_REPO_PATH not set!')
+
+    repo_path = os.environ['HEXRD_EXAMPLE_REPO_PATH']
+    return Path(repo_path)
--- tests/test_find_orientations.py
+++ tests/test_find_orientations.py
@@ -26,15 +26,6 @@ root.addHandler(handler)
 
 
 @pytest.fixture
-def example_repo_path():
-    if 'HEXRD_EXAMPLE_REPO_PATH' not in os.environ:
-        pytest.fail('Environment varable HEXRD_EXAMPLE_REPO_PATH not set!')
-
-    repo_path = os.environ['HEXRD_EXAMPLE_REPO_PATH']
-    return Path(repo_path)
-
-
-@pytest.fixture
 def example_repo_include_path(example_repo_path):
     return example_repo_path / 'NIST_ruby' / 'multiruby_dexelas' / 'include'
 
