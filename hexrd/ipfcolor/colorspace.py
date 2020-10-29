f7bc1c6fe8a079df8642d4ce78aad6b15c59c6c9
Saransh <saransh1@llnl.gov>
2020-10-29 16:26:20 -0700

    more additions to the core computation routines. slowly adding the angle computation for the spherical sector.

--- hexrd/ipfcolor/colorspace.py
+++ hexrd/ipfcolor/colorspace.py
@@ -1,55 +1,149 @@
-def hsl_rgb(hsl):
-	'''
-	first check the shape of the hsl array. it has to be nx3 array
-	it is assumed that the hsl array has values in [0,1] for the 
-	different components
-	'''
-	hsl = np.contiguousarray( np.atleast_2d(hsl) )
-	if(hsl.ndim != 2):
-		raise RuntimeError("hsl_rgb: shape of hsl array is invalid.")
-	rgb = np.zeros(hsl.shape)
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Copyright (c) 2020, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by Saransh Singh <saransh1@llnl.gov> and others.
+# LLNL-CODE-529294.
+# All rights reserved.
+#
+# This file is part of HEXRD. For details on dowloading the source,
+# see the file COPYING.
+#
+# Please also see the file LICENSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program (see file LICENSE); if not, write to
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307 USA or visit <http://www.gnu.org/licenses/>.
+# =============================================================================
 
-	'''
-	calculate the different factors needed for the conversion
-	'''
-	H = hsl[:,0]
-	S = hsl[:,1]
-	L = hsl[:,2]
+from hexrd import constants
 
-	C = (1.0 - np.abs(2.*L - 1.)) * S
-	X = (1.0 - np.abs(np.mod(6*H,2) - 1.0) ) * C
-	m = L - C/2.
+eps = constants.sqrt_epsf
 
-	case = np.floor(6.*H).astype(np.int32)
 
-	'''
-	depending on the range of H, the rgb definition changes. see
-	https://www.rapidtables.com/convert/color/hsl-to-rgb.html
-	for the detailed formula
-	'''
-	Cp = np.atleast_2d(C+m).T
-	Xp = np.atleast_2d(X+m).T
-	Zp = np.atleast_2d(m).T
+def hsl2rgb(hsl):
+    '''
+    >> @AUTHOR:     Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+    >> @DATE:       10/29/2020 SS 1.0 original
+    >> @DETAILS:    routines to convert between hsl to rgb representations, the
+                    input and output will have all components in the range [0,1]
+    '''
 
-	if(case == 0):
-		mask = case == 0
-		rgb[mask,:] = np.hstack((Cp[mask,:], Xp[mask,:], Zp[mask,:]))
-	elif(case == 1):
-		mask = case == 1
-		rgb[mask,:] = np.hstack((Xp[mask,:], Cp[mask,:], Zp[mask,:])) 
-	elif(Case == 2):
-		mask = case == 2
-		rgb[mask,:] = np.hstack((Zp[mask,:], Cp[mask,:], Xp[mask,:])) 
-	elif(case == 3):
-		mask = case == 3
-		rgb[mask,:] = np.hstack((Zp[mask,:], Xp[mask,:], Cp[mask,:])) 
-	elif(case == 4):
-		mask = case == 4
-		rgb[mask,:] = np.hstack((Xp[mask,:], Zp[mask,:], Cp[mask,:])) 
-	elif(case == 5):
-		mask = case == 5
-		rgb[mask,:] = np.hstack((Cp[mask,:], Zp[mask,:], Xp[mask,:])) 
-	else:
-		raise RuntimeError("value of Hue is not in range [0,1]")
+    '''
+    first check the shape of the hsl array. it has to be nx3 array
+    it is assumed that the hsl array has values in [0,1] for the 
+    different components
+    '''
+    hsl = np.contiguousarray(np.atleast_2d(hsl))
+    if(hsl.ndim != 2):
+        raise RuntimeError("hsl_rgb: shape of hsl array is invalid.")
+    rgb = np.zeros(hsl.shape)
 
-	return rgb
+    '''
+    calculate the different factors needed for the conversion
+    '''
+    H = hsl[:, 0]
+    S = hsl[:, 1]
+    L = hsl[:, 2]
+
+    C = (1.0 - np.abs(2.*L - 1.)) * S
+    X = (1.0 - np.abs(np.mod(6*H, 2) - 1.0)) * C
+    m = L - C/2.
+
+    case = np.floor(6.*H).astype(np.int32)
+
+    '''
+    depending on the range of H, the rgb definition changes. see
+    https://www.rapidtables.com/convert/color/hsl-to-rgb.html
+    for the detailed formula
+    '''
+    Cp = np.atleast_2d(C+m).T
+    Xp = np.atleast_2d(X+m).T
+    Zp = np.atleast_2d(m).T
+
+    if(case == 0):
+        mask = case == 0
+        rgb[mask, :] = np.hstack((Cp[mask, :], Xp[mask, :], Zp[mask, :]))
+    elif(case == 1):
+        mask = case == 1
+        rgb[mask, :] = np.hstack((Xp[mask, :], Cp[mask, :], Zp[mask, :]))
+    elif(Case == 2):
+        mask = case == 2
+        rgb[mask, :] = np.hstack((Zp[mask, :], Cp[mask, :], Xp[mask, :]))
+    elif(case == 3):
+        mask = case == 3
+        rgb[mask, :] = np.hstack((Zp[mask, :], Xp[mask, :], Cp[mask, :]))
+    elif(case == 4):
+        mask = case == 4
+        rgb[mask, :] = np.hstack((Xp[mask, :], Zp[mask, :], Cp[mask, :]))
+    elif(case == 5):
+        mask = case == 5
+        rgb[mask, :] = np.hstack((Cp[mask, :], Zp[mask, :], Xp[mask, :]))
+    else:
+        raise RuntimeError("value of Hue is not in range [0,1]")
+
+    return rgb
+
+
+def rgb2hsl(rgb):
+    '''
+    >> @AUTHOR:     Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+    >> @DATE:       10/29/2020 SS 1.0 original
+    >> @DETAILS:    routines to convert between rgb to hsl representations, the
+                    input and output will have all components in the range [0,1]
+    '''
+    '''
+    first check the shape of the rgb array. it has to be nx3 array
+    it is assumed that the hsl array has values in [0,1] for the 
+    different components
+    '''
+    rgb = np.contiguousarray(np.atleast_2d(rgb))
+    if(rgb.ndim != 2):
+        raise RuntimeError("hsl_rgb: shape of hsl array is invalid.")
+    hsl = np.zeros(rgb.shape)
+
+    '''
+    calculate different factors needed for the conversion
+    '''
+    Cmax = rgb.max(axis=1)
+    Cmin = rgb.min(axis=1)
+    delta = Cmax - Cmin
+
+    L = 0.5 * (Cmax + Cmin)
+
+    # catching cases where delta is close to zero
+    zmask = np.abs(delta) < eps
+    hsl[zmask, 2] = L[zmask]
+
+    # depending on whether r,g or b is maximum, the hue is
+    # assigned different values, we will deal with those cases here
+
+    rmask = rgb[:, 0] == Cmax
+    rmask = np.logical_and(rmask, np.logical_not(zmask))
+    hsl[rmask, 0] = np.mod(
+        (rgb[rmask, 1] - rgb[rmask, 2])/delta[rmask], 6) / 6.
+
+    gmask = rgb[:, 1] == Cmax
+    gmask = np.logical_and(gmask, np.logical_not(zmask))
+    hsl[gmask, 0] = np.mod(
+        (rgb[gmask, 2] - rgb[gmask, 0])/delta[gmask] + 2., 6) / 6.
+
+    bmask = rgb[:, 2] == Cmax
+    bmask = np.logical_and(bmask, np.logical_not(zmask))
+    hsl[bmask, 0] = np.mod(
+        (rgb[bmask, 0] - rgb[bmask, 1])/delta[bmask] + 4., 6) / 6.
+
+    hsl[np.logical_not(zmask), 1] = delta[np.logical_not(
+        zmask)] / (1. - np.abs(2 * L[np.logical_not(zmask)] - 1.))
+
+    return hsl
--- hexrd/ipfcolor/sphere_sector.py
+++ hexrd/ipfcolor/sphere_sector.py
@@ -1,67 +1,210 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Copyright (c) 2020, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by Saransh Singh <saransh1@llnl.gov> and others.
+# LLNL-CODE-529294.
+# All rights reserved.
+#
+# This file is part of HEXRD. For details on dowloading the source,
+# see the file COPYING.
+#
+# Please also see the file LICENSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program (see file LICENSE); if not, write to
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307 USA or visit <http://www.gnu.org/licenses/>.
+# =============================================================================
+from hexrd import constants
+
+eps = constants.sqrt_epsf
+
+'''
+In this section we will list the vertices of the spherical triangles
+for each of the point group. this will be in the form of a dictionary
+conforming to the symbols in unitcell.py
+'''
+pg2vertex = {
+    'c1' : []
+    'ci' : []
+    'c2' : []
+    'cs' : []
+    'c2h' : []
+    'd2' : []
+    'c2v' : []
+    'd2h' : []
+    'c4' : []
+    's4' : []
+    'c4h' : []
+    'd4' : []
+    'c4v' : []
+    'd2d' : []
+    'd4h' : []
+    'c3' : []
+    's6' : []
+    'd3' : []
+    'c3v' : []
+    'd3d' : []
+    'c6' : []
+    'c3h' : []
+    'c6h' : []
+    'd6' : []
+    'c6v' : []
+    'd3h' : []
+    'd6h' : []
+    't' : []
+    'th' : []
+    'o' : []
+    'td' : []
+    'oh' : []
+}
+
 class sector
-'''
-	@AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
-	@DATE    10/28/2020 SS 1.0 original
-	@DETAIL  this class is used to store spherical patch for a given point group.
-			 the class also has methods to compute the color of a direction by
-			 computing the hue, saturation and lightness values in [0,1]. these
-			 values can be converted to rgb for display with the well known 
-			 conversion formula.
+    '''
+    @AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+    @DATE    10/28/2020 SS 1.0 original
+    @DETAIL  this class is used to store spherical patch for a given point group.
+             the class also has methods to compute the color of a direction by
+             computing the hue, saturation and lightness values in [0,1]. these
+             values can be converted to rgb for display with the well known 
+             conversion formula.
 
 
-	All the methodology and equations have been taken from the paper:
-	Orientations – perfectly colored, G. Nolze and R. Hielscher, J. Appl. Cryst. (2016). 49, 1786–1802
+    All the methodology and equations have been taken from the paper:
+    Orientations – perfectly colored, G. Nolze and R. Hielscher, J. Appl. Cryst. (2016). 49, 1786–1802
 
-'''
-def __init__(self, vertex, center):
-	pass
+    '''
+    def __init__(self, pgsym):
+        '''
+        initialize the class with a vertex and the barycenter
+        the vertes are organized as columns
+        '''
+        vertex = pg2vertex[pgsym]
 
-def check_hemisphere(self):
-	pass
+        self.vx = vertex[:,0]
+        self.vy = vertex[:,1]
+        self.vz = vertex[:,2]
 
-def calc_azi_rho(di3):
-	'''
-	@AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
-	@DATE    10/28/2020 SS 1.0 original
-	@PARAM   dir3 direction in fundamental sector. behavior is undefined if
-		     direction is outside the fundamental sector
-	@DETAIL  this function is used to calculate the azimuthal angle of the
-			 direction inside a spherical patch. this is computed as the angle
-			 with the vector defined by the first vertex and barycenter and the
-			 vector defined by direction and barycenter.
+        '''
+        make sure there are unit norm
+        '''
+        nvx = np.linalg.norm(self.vx)
+        nvy = np.linalg.norm(self.vy)
+        nvz = np.linalg.norm(self.vz)
+        
+        if(np.abs(nvx) > eps):
+            self.vx /= nvx
+        else:
+            raise RuntimeError("one of the spherical vertex is null.")
 
-	'''
-	pass
+        if(np.abs(nvy) > eps):
+            self.vy /= nvy
+        else:
+            raise RuntimeError("one of the spherical vertex is null.")
 
-def calc_pol_theta(dir3):
-	'''
-	@AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
-	@DATE    10/28/2020 SS 1.0 original
-	@PARAM   dir3 direction in fundamental sector. behavior is undefined if
-		     direction is outside the fundamental sector
-	@DETAIL  this function is used to calculate the polar angle of the
-			 direction inside a spherical patch. this is computed as the scaled
-			 angular distance between direction and barycenter. the scaling is such
-			 that the boundary is 90 degrees from barycenter.
+        if(np.abs(nvz) > eps):
+            self.vz /= nvz
+        else:
+            raise RuntimeError("one of the spherical vertex is null.")
 
-	'''
-	pass
+        '''
+        check if all the vertices are in the same hemisphere
+        get the z-components of the vertices
+        '''
+        self.check_hemisphere()
 
-def calc_hue(self, rho):
-	'''
-	@AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
-	@DATE    10/28/2020 SS 1.0 original
-	@PARAM   dir3 direction in fundamental sector. behavior is undefined if
-		     direction is outside the fundamental sector
-	@DETAIL  this function is used to calculate the hue based on angular coordinates
-			 of direction within the spherical patch. the velocity
+        # compute the barycenter or the centroid
+        self.barycenter = (self.vx + self.vy + self.vz) / 3.
+        self.barycenter /= np.linalg.norm(self.barycenter)
 
-	'''
+        # this is the vector about which the azimuthal angle is 
+        # computed (vx - barycenter)
+        self.rx = self.vx - self.barycenter
+        self.rx /= np.linalg.norm(self.rx)
 
-	pass
+    def check_norm(self, dir3):
+        '''
+        @AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+        @DATE    10/29/2020 SS 1.0 original
+        @PARAM   dir3 direction in fundamental sector. size is nx3
+        @DETAIL  this function is used to make sure the directions are all unit norm
 
-def calc_saturation(self):
-	pass
+        '''
+        n = np.linalg.norm(dir3, axis=1)
+        mask = n > eps
+        n = n[mask]
+        dir3[mask,:] = dir3[mask,:]/np.tile(n,[3,1]).T
 
-def calc_lightness(self, theta):
-	pass
+    def check_hemisphere(self):
+
+        zcoord = np.array([self.vx[2], self.vy[2], self.vz[2]])
+        if(np.logical_or( np.all(zcoord > 0.),  np.all(zcoord < 0.) ) ):
+            pass
+        else:
+            raise RuntimeError("sphere_sector: the vertices of the stereographic \
+                triangle are not in the same hemisphere")
+
+    def calc_azi_rho(self, di3):
+        '''
+        @AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+        @DATE    10/28/2020 SS 1.0 original
+        @PARAM   dir3 direction in fundamental sector. behavior is undefined if
+                 direction is outside the fundamental sector. size is nx3
+        @DETAIL  this function is used to calculate the azimuthal angle of the
+                 direction inside a spherical patch. this is computed as the angle
+                 with the vector defined by the first vertex and barycenter and the
+                 vector defined by direction and barycenter.
+
+        '''
+        '''
+        first make sure all the directions are unit normal
+        if not make them unit normals
+        the directions with zero norms are ignored
+        '''
+        self.check_norm(dir3)
+        c = np.dot(self.rx, dia3.T)
+        s = np.cross(np.tile(self.rx,[dir3.shape[0],1]), dir3)
+        rho = np.arctan2(s, c)
+
+    def calc_pol_theta(dir3):
+        '''
+        @AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+        @DATE    10/28/2020 SS 1.0 original
+        @PARAM   dir3 direction in fundamental sector. behavior is undefined if
+                 direction is outside the fundamental sector
+        @DETAIL  this function is used to calculate the polar angle of the
+                 direction inside a spherical patch. this is computed as the scaled
+                 angular distance between direction and barycenter. the scaling is such
+                 that the boundary is 90 degrees from barycenter.
+
+        '''
+        pass
+
+    def calc_hue(self, rho):
+        '''
+        @AUTHOR  Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+        @DATE    10/28/2020 SS 1.0 original
+        @PARAM   dir3 direction in fundamental sector. behavior is undefined if
+                 direction is outside the fundamental sector
+        @DETAIL  this function is used to calculate the hue based on angular coordinates
+                 of direction within the spherical patch. the velocity
+
+        '''
+
+        pass
+
+    def calc_saturation(self):
+        pass
+
+    def calc_lightness(self, theta):
+        pass
