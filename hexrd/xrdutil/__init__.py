35809c1f7733f931aa38b2dcfbcee93eacada5f4
Chris Harris <chris.harris@kitware.com>
2021-02-26 16:26:45 -0500

    Fix up private imports

--- hexrd/xrdutil/__init__.py
+++ hexrd/xrdutil/__init__.py
@@ -1,1 +1,6 @@
 from .utils import *
+
+# For now import these private members!
+from .utils import _project_on_detector_plane
+from .utils import _fetch_hkls_from_planedata
+from .utils import _filter_hkls_eta_ome
