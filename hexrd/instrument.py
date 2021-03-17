e0721c8d25d87ee5e0dbb190be93785a7fac002f
Joel Bernier <joelvbernier@me.com>
2021-03-17 15:35:17 -0700

    moving back inside loop to stop memory explosion

--- hexrd/instrument.py
+++ hexrd/instrument.py
@@ -937,14 +937,13 @@ class HEDMInstrument(object):
                             pass
                         pass
 
-                    # handle threshold if specified
-                    this_det_image = np.array(imgser_dict[det_key])
-                    if threshold is not None:
-                        # !!! NaNs get preserved
-                        this_det_image[this_det_image < threshold] = 0.
-
                     # histogram intensities over eta ranges
-                    for i_row, image in enumerate(this_det_image):
+                    for i_row, image in enumerate(imgser_dict[det_key]):
+                        # handle threshold if specified
+                        if threshold is not None:
+                            # !!! NaNs get preserved
+                            image = np.array(image)
+                            image[image < threshold] = 0.
                         if fast_histogram:
                             def _on_done(map, row, reta, future):
                                 map[row, reta] = future.result()
