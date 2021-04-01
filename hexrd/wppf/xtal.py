6c19f99c3cb8e0ab5e36d70c6c12fb1113a399a7
Saransh <saransh1@llnl.gov>
2021-03-31 18:53:04 -0700

    Fixing the error in Rietveld.

--- hexrd/wppf/xtal.py
+++ hexrd/wppf/xtal.py
@@ -15,18 +15,18 @@ def _calc_dspacing(rmt, hkls):
 @numba_njit_if_available(cache=True, nogil=True)
 def _get_tth(dsp, wavelength):
     nn = dsp.shape[0]
-    tth = np.zeros([nn,])
-    wavelength_allowed_hkls = np.zeros([nn,], dtype=bool)
-    for ii in nn:
+    tth = np.zeros(dsp.shape[0])
+    wavelength_allowed_hkls = np.zeros(dsp.shape[0])
+    for ii in np.arange(nn):
         d = dsp[ii]
         glen = 1./d
         sth = glen*wavelength/2.
         if(np.abs(sth) <= 1.0):
             t = 2. * np.degrees(np.arcsin(sth))
-            tth[i] = t
-            wavelength_allowed_hkls[ii] = True
+            tth[ii] = t
+            wavelength_allowed_hkls[ii] = 1
         else:
             tth[ii] = np.nan
-            wavelength_allowed_hkls[ii] = False
+            wavelength_allowed_hkls[ii] = 0
 
     return tth, wavelength_allowed_hkls
