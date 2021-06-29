1955ae4fc74c278f814118d02bbdccadec74b630
Saransh Singh <saransh13@gmail.com>
2021-06-29 15:08:54 -0700

    working mesh class which can interpolate harmonic values from the mesh. a stereographic projection is used to simplify the process

--- hexrd/wppf/texture.py
+++ hexrd/wppf/texture.py
@@ -21,7 +21,7 @@ import h5py
 
     >> @PARAMETERS:     
 """
-class texture:
+class mesh_s2:
     """
     ========================================================================================================
     ========================================================================================================
@@ -59,15 +59,61 @@ class texture:
         data = importlib.resources.open_binary(hexrd.resources, "surface_harmonics.h5")
         with h5py.File(data, 'r') as fid:
             
+            gname = f"{symmetry}"
+            self.symmetry = symmetry
+            self.ncrd = fid[gname].attrs["ncrd"]
+            self.neqv = fid[gname].attrs["neqv"]
+            self.nindp = fid[gname].attrs["nindp"]
+
             dname = f"/{symmetry}/crd"
+
             pts = np.array(fid[dname])
             n = np.linalg.norm(pts, axis=1)
             self.points = pts/np.tile(n, [3,1]).T
 
+            points_st = self.points[:,:2]/np.tile(
+                (1.+self.points[:,2]),[2,1]).T
+
             dname = f"/{symmetry}/harmonics"
             self.harmonics = np.array(fid[dname])
 
-            self.mesh = Delaunay(self.points, qhull_options="QJ")
+            dname = f"/{symmetry}/eqv"
+            self.eqv = np.array(fid[dname]).astype(np.int32)
+
+            self.mesh = Delaunay(points_st, qhull_options="QJ")
+
+    def _get_simplices(self,
+                       points):
+        """
+        this function is used to get the index of the simplex
+        in which the point lies. this is the first step to 
+        calculating the barycentric coordinates, which are the
+        weights to linear interpolation.
+        """
+
+        n = np.linalg.norm(points, axis=1)
+        points = points/np.tile(n, [3,1]).T
+
+        points_st = points[:,:2]/np.tile(
+                (1.+points[:,2]),[2,1]).T
+
+        simplices = self.mesh.find_simplex(points_st)
+
+        """
+        there might be some points which end up being slighty outside
+        the circle due to numerical precision. just scale those points
+        by a number close to one to bring them closer to the origin.
+        the program should ideally never get here
+        """
+        mask = (simplices == -1)
+        simplices[mask] = self.mesh.find_simplex(points_st[mask,:]*0.995)
+
+        if -1 in simplices:
+            msg = (f"some points seem to not be in the"
+            f"mesh. please check input")
+            raise RuntimeError(msg)
+
+        return simplices
 
     def _get_barycentric_coordinates(self,
                                     points):
@@ -81,11 +127,110 @@ class texture:
 
         first make sure that the points are all normalized
         to unit length
+
+        then take the stereographic projection
         """
         n = np.linalg.norm(points, axis=1)
         points = points/np.tile(n, [3,1]).T
+
+        points_st = points[:,:2]/np.tile(
+                (1.+points[:,2]),[2,1]).T
 
         """
         next get the simplices. a value of -1 is returned
         when the point can not be found inside any simplex.
         those points will be handled separately
+        """
+        simplices = self._get_simplices(points)
+
+        """
+        we have the simplex index for each point now. move on to 
+        computing the barycentric coordinates. the logic is simple
+        inverse of the T matrix is in mesh.transforms, we use that
+        to compute T^{-1}(r-r3)
+        see wikipedia for more details
+        """
+        bary_center = [self.mesh.transform[simplices[i],:2].dot(
+        (np.transpose(points_st[i,:] - 
+        self.mesh.transform[simplices[i],2]))) 
+        for i in np.arange(points.shape[0])]
+        
+        bary_center = np.array(bary_center)
+
+        """
+        the fourth coordinate is 1 - sum of the other three
+        """
+        bary_center = np.hstack((bary_center, 
+        1. - np.atleast_2d(bary_center.sum(axis=1)).T))
+
+        bary_center[np.abs(bary_center)<1e-9] = 0.
+
+        return np.array(bary_center), simplices
+
+    def _get_equivalent_node(self,
+                             node_id):
+        """
+        given the index of the node, find out the
+        equivalent node. if the node is already one
+        of the independent nodes, then the same value
+        is returned. Note that the index is all 1 based
+        in the surface harmonic file and all 0 based in 
+        the scipy Delaunay function
+        """
+        mask = node_id+1 > self.nindp
+        eqv_node_id = np.zeros(node_id.shape).astype(np.int32)
+
+        """
+        for the independent node, return the same value
+        """
+        eqv_node_id[~mask] = node_id[~mask]
+
+        """
+        for the other node, calculate the equivalent index
+        using the eqv array
+        """
+        if not np.all(mask == False):
+            eqv_id = np.array([np.where(self.eqv[:,0] == i) 
+                for i in node_id[mask]]).astype(np.int32)
+            eqv_id = np.squeeze(eqv_id)
+            eqv_node_id[mask] = self.eqv[eqv_id,1]-1
+
+        return eqv_node_id
+
+
+    def _get_harmonic_values(self,
+                            points):
+        """
+        this is the main function which compute the
+        value of the harmonic function at a given set 
+        of points using linear interpolation of the 
+        values on the fem mesh. the sequence of function
+        calls is as follows:
+
+        1. compute the barycentric coordinates
+        2. compute the equivalent nodes 
+        3. perform weighted mean
+
+        the value of harmonics upto the nodal degree of
+        freedom is return. the user can then select how many
+        to use and where to truncate
+        """
+        bary_center, simplex_id = self._get_barycentric_coordinates(points)
+        node_id = self.mesh.simplices[simplex_id]
+
+        eqv_node_id = np.array([self._get_equivalent_node(nid) 
+            for nid in node_id]).astype(np.int32)
+
+        fval = np.array([self.harmonics[nid,:] for nid in eqv_node_id])
+        nharm = self.harmonics.shape[1]
+
+        fval_points = np.array([np.sum(np.tile(bary_center[i,:],[nharm,1]).T*
+            fval[i,:,:],axis=0) for i in range(points.shape[0])])
+
+        return fval_points
+
+
+
+
+
+
