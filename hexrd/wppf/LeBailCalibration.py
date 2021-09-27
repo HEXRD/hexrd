a1c2666d1680fce168022e8f7e868062d6a2449c
Saransh Singh <saransh13@gmail.com>
2021-09-27 14:45:59 -0700

    working code. needs to be throroughly tested.

--- hexrd/wppf/LeBailCalibration.py
+++ hexrd/wppf/LeBailCalibration.py
@@ -1,5 +1,7 @@
 import importlib.resources
 import numpy as np
+from numpy.polynomial.chebyshev import Chebyshev
+import lmfit
 import warnings
 from hexrd.wppf.peakfunctions import \
 calc_rwp, computespectrum_pvfcj, \
@@ -9,7 +11,8 @@ calc_Iobs_pvfcj,\
 calc_Iobs_pvtch,\
 calc_Iobs_pvpink
 from hexrd.wppf.spectrum import Spectrum
-from hexrd.wppf import wppfsupport, LeBail, parameters
+from hexrd.wppf import wppfsupport, LeBail
+from hexrd.wppf.parameters import Parameters
 from hexrd.wppf.phase import Phases_LeBail, Material_LeBail
 from hexrd.imageutil import snip1d, snip1d_quad
 from hexrd.material import Material
@@ -53,7 +56,7 @@ class LeBailCalibrator:
                  params=None,
                  phases=None,
                  azimuthal_step=5.0,
-                 bkgmethod={'spline': None},
+                 bkgmethod={'chebyshev': 3},
                  peakshape="pvtch",
                  intensity_init=None,
                  apply_solid_angle_correction=False,
@@ -82,6 +85,8 @@ class LeBailCalibrator:
         self.intensity_init = intensity_init
 
         self.initialize_Icalc()
+
+        self.calc_simulated()
 
         self._tstop = time.time()
         
@@ -132,13 +137,15 @@ class LeBailCalibrator:
             for p in self.phases:
                 Icalc[p] = {}
                 for k, l in self.phases.wavelength.items():
-                    Icalc[p][k] = np.zeros(self.tth[p][k].shape)
+                    Icalc[p][k] = 100.0*np.ones(self.tth[p][k].shape)
 
             prefix = f"azpos{ii}"
             self.Icalc[prefix] = Icalc
             wppfsupport._add_intensity_parameters(self.params,self.hkls,Icalc,prefix)
 
         self.refine_intensities = False
+        self.refine_background = False
+        self.refine_instrument = False
 
 
     def prepare_polarview(self):
@@ -164,6 +171,35 @@ class LeBailCalibrator:
                 self.lineouts[key] = data
 
 
+    def computespectrum(self,
+                        instr_updated,
+                        lp_updated):
+        """
+        this function calls the computespectrum function in the 
+        lebaillight class for all the azimuthal positions and 
+        accumulates the error vector from each of those lineouts.
+        this is more or less a book keeping function rather
+        """
+        errvec = np.empty([0,])
+        for k,v in self.lineouts_sim.items():
+            if instr_updated:
+                v.lineout = self.lineouts[k]
+            if lp_updated:
+                v.tth = self.tth
+                v.hkls = self.hkls
+                v.dsp = self.dsp
+            v.shkl = self.shkl
+            v.params = self.params
+
+            v.computespectrum()
+            ww = v.weights
+            evec = ww*(v.spectrum_expt.data_array[:,1] -
+                   v.spectrum_sim.data_array[:,1])**2
+            evec = np.sqrt(evec)
+            errvec = np.concatenate((errvec,evec))
+
+        return errvec
+
     def calcrwp(self, params):
         """
         this is one of the main functions which differs fundamentally
@@ -173,9 +209,142 @@ class LeBailCalibrator:
         of omega. However, there will be support for the more complicated
         HEDM case in the future.
         """
-        self.set_params_vals_to_class(params)
+        lp_updated = self.update_param_vals(params)
+        self.update_shkl(params)
+        instr_updated = self.update_instrument(params)
+        errvec = self.computespectrum(instr_updated,
+                                      lp_updated)
 
-        # return errvec
+        print(np.sum(errvec))
+
+        return errvec
+
+
+    def initialize_lmfit_parameters(self):
+
+        params = lmfit.Parameters()
+
+        for p in self.params:
+            par = self.params[p]
+            if(par.vary):
+                params.add(p, value=par.value, min=par.lb, max=par.ub)
+
+        return params
+
+    def Refine(self):
+        """
+        >> @AUTHOR:     Saransh Singh, Lawrence Livermore National Lab, saransh1@llnl.gov
+        >> @DATE:       05/19/2020 SS 1.0 original
+        >> @DETAILS:    this routine performs the least squares refinement for all variables
+                        which are allowed to be varied.
+        """
+
+        params = self.initialize_lmfit_parameters()
+
+        fdict = {'ftol': 1e-6, 'xtol': 1e-6, 'gtol': 1e-6,
+         'verbose': 0, 'max_nfev': 1000, 'method':'trf',
+         'jac':'2-point'}
+        fitter = lmfit.Minimizer(self.calcrwp, params)
+
+        res = fitter.least_squares(**fdict)
+        return res
+
+    def update_param_vals(self,
+                          params):
+        """
+        @date 03/12/2021 SS 1.0 original
+        take values in parameters and set the
+        corresponding class values with the same
+        name
+        """
+        for p in params:
+            if params[p].vary:
+                if p in self.params:
+                    self.params[p].value = params[p].value
+                    self.params[p].lb = params[p].min
+                    self.params[p].ub = params[p].max
+
+
+        updated_lp = False
+
+        for p in self.phases:
+            mat = self.phases[p]
+            """
+            PART 1: update the lattice parameters
+            """
+            lp = []
+            lpvary = False
+            pre = p + '_'
+
+            name = [f"{pre}{x}" for x in wppfsupport._lpname]
+
+            for nn in name:
+                if nn in params:
+                    if params[nn].vary:
+                        lpvary = True
+                    lp.append(params[nn].value)
+                elif nn in self.params:
+                    lp.append(self.params[nn].value)
+
+            if(not lpvary):
+                pass
+            else:
+                lp = self.phases[p].Required_lp(lp)
+                mat.lparms = np.array(lp)
+                mat._calcrmt()
+                updated_lp = True
+
+        if updated_lp:
+            self.calctth()
+
+        return updated_lp
+
+    def update_shkl(self, params):
+        """
+        if certain shkls are refined, then update
+        them using the params arg. else use values from
+        the parameter class
+        """
+        updated_shkl = False
+        shkl_dict = {}
+        for p in self.phases:
+            shkl_name = self.phases[p].valid_shkl
+            eq_const = self.phases[p].eq_constraints
+            mname = self.phases[p].name
+            key = [f"{mname}_{s}" for s in shkl_name]
+            for s,k in zip(shkl_name,key):
+                if k in params:
+                    shkl_dict[s] = params[k].value
+                else:
+                    shkl_dict[s] = self.params[k].value
+
+            self.phases[p].shkl = wppfsupport._fill_shkl(\
+                shkl_dict, eq_const)
+
+    def update_instrument(self, params):
+        instr_updated = False
+        for key,det in self._instrument.detectors.items():
+            for ii in range(3):
+                pname = f"{key}_tvec{ii}"
+                if pname in params:
+                    if params[pname].vary:
+                        det.tvec[ii] = params[pname].value
+                        instr_updated = True
+                pname = f"{key}_tilt{ii}"
+                if pname in params:
+                    if params[pname].vary:
+                        det.tilt[ii] = params[pname].value
+                        instr_updated = True
+
+        if instr_updated:
+            self.prepare_polarview()
+        return instr_updated
+
+    @property
+    def bkgdegree(self):
+        if "chebyshev" in self.bkgmethod.keys():
+            return self.bkgmethod["chebyshev"]
+    
 
     @property
     def instrument(self):
@@ -257,6 +426,9 @@ class LeBailCalibrator:
         lam = keVToAngstrom(self.instrument.beam_energy)
         return {"lam1": 
                 [valWUnit('lp', 'length', lam, 'angstrom'),1.0]}
+
+    def striphkl(self, g):
+        return str(g)[1:-1].replace(" ","") 
     
     @property
     def refine_intensities(self):
@@ -271,14 +443,48 @@ class LeBailCalibrator:
                 for p in Icalc:
                     for k in Icalc[p]:
                         shape = Icalc[p][k].shape[0]
-                        pname = [f"{prefix}{ii}_{p}_{k}_I{g}" 
+                        pname = [f"{prefix}{ii}_{p}_{k}_I{self.striphkl(g)}" 
                         for jj,g in zip(range(shape),self.hkls[p][k])]
                         for jj in range(shape):
                             self.params[pname[jj]].vary = val
         else:
             msg = "only boolean values accepted"
             raise ValueError(msg)
+
+    @property
+    def refine_background(self):
+        return self._refine_background
     
+    @refine_background.setter
+    def refine_background(self, val):
+        if isinstance(val, bool):
+            self._refine_background = val
+            prefix = "azpos"
+            for ii in range(len(self.lineouts)):
+                pname = [f"{prefix}{ii}_bkg_C{jj}" for jj in range(self.bkgdegree)]
+                for p in pname:
+                    self.params[p].vary = val
+        else:
+            msg = "only boolean values accepted"
+            raise ValueError(msg)
+
+    @property
+    def refine_instrument(self):
+        return self._refine_instrument
+    
+    @refine_instrument.setter
+    def refine_instrument(self, val):
+        if isinstance(val, bool):
+            self._refine_instrument = val
+            for key in self.instrument.detectors:
+                pnametvec = [f"{key}_tvec{i}" for i in range(3)]
+                pnametilt = [f"{key}_tilt{i}" for i in range(3)]
+                for ptv,pti in zip(pnametvec,pnametilt):
+                    self.params[ptv].vary = val
+                    self.params[pti].vary = val
+        else:
+            msg = "only boolean values accepted"
+            raise ValueError(msg)
     @property
     def pixel_size(self):
         return self._pixel_size
@@ -567,7 +773,30 @@ class LeBailCalibrator:
                 self.phases, self.peakshape)
             self.lebail_param_list = [p for p in params]
             wppfsupport._add_detector_geometry(params, self.instrument)
+            wppfsupport._add_background(params, self.lineouts, self.bkgdegree)
             self._params = params
+
+    @property
+    def shkl(self):
+        shkl = {}
+        for p in self.phases:
+            shkl[p] = self.phases[p].shkl
+        return shkl
+    
+
+    def calc_simulated(self):
+        self.lineouts_sim = {}
+        for key, lo in self.lineouts.items():
+            self.lineouts_sim[key] = LeBaillight(key,
+                                                 lo,
+                                                 self.tth,
+                                                 self.hkls,
+                                                 self.dsp,
+                                                 self.shkl,
+                                                 self.lebail_param_list,
+                                                 self.params,
+                                                 self.peakshape,
+                                                 self.bkgmethod)
 
 class LeBaillight:
     """
@@ -584,17 +813,20 @@ class LeBaillight:
                 shkl,
                 lebail_param_list,
                 params,
-                peakshape):
+                peakshape,
+                bkgmethod):
 
         self.name = name
         self.lebail_param_list = lebail_param_list
         self.peakshape = peakshape
         self.params = params
-        self.lineout = lineout[name]
+        self.lineout = lineout
         self.shkl = shkl
         self.tth = tth
         self.hkls = hkls
         self.dsp = dsp
+        self.bkgmethod = bkgmethod
+        self.computespectrum()
 
     def computespectrum(self):
         
@@ -694,16 +926,49 @@ class LeBaillight:
 
         self._spectrum_sim = Spectrum(x=x, y=y)
 
+    @property
+    def weights(self):
+        lo = self.lineout
+        mask = self.mask[:,1]
+        weights = 1./np.sqrt(lo.data[:,1])
+        weights[mask] = np.nan
+        return self._weights
+    
+
+    @property
+    def bkgdegree(self):
+        if "chebyshev" in self.bkgmethod.keys():
+            return self.bkgmethod["chebyshev"]
+
+    @property
+    def background(self):
+        tth, I = self._spectrum_sim.data
+        mask = self.mask[:,1]
+    
+        pname = [f"{self.name}_bkg_C{ii}" 
+        for ii in range(self.bkgdegree)]
+
+        coef = [self.params[p].value for p in pname]
+        c = Chebyshev(coef,domain=[tth[0],tth[-1]])
+        bkg = c(tth)
+        bkg[mask] = np.nan
+        return bkg
 
     @property
     def spectrum_sim(self):
         tth, I = self._spectrum_sim.data
         mask = self.mask[:,1]
         I[mask] = np.nan
-        # I += self.background.y
+        I += self.background
 
         return Spectrum(x=tth, y=I)
     
+    @property
+    def spectrum_expt(self):
+        d = self.lineout.data
+        mask = self.mask[:,1]
+        d[mask,1] = np.nan
+        return Spectrum(x=d[:,0], y=d[:,1])    
 
     @property
     def params(self):
@@ -719,7 +984,7 @@ class LeBaillight:
         xn, wn = roots_legendre(16)
         self.xn = xn[8:]
         self.wn = wn[8:]
-        if isinstance(params, parameters.Parameters):
+        if isinstance(params, Parameters):
             if hasattr(self, 'params'):
                 for p in params:
                     if (p in self.lebail_param_list) or (self.name in p):
@@ -728,7 +993,7 @@ class LeBaillight:
                         self._params[p].lb = params[p].lb
                         self._params[p].vary = params[p].vary
             else:
-                self._params = parameters.Parameters()
+                self._params = Parameters()
                 for p in params:
                     if (p in self.lebail_param_list) or (self.name in p):
                         self._params.add(name=p,
@@ -737,10 +1002,10 @@ class LeBaillight:
                                             lb=params[p].lb,
                                             vary=params[p].vary)
 
-            if hasattr(self, "tth") and \
-            hasattr(self, "dsp") and \
-            hasattr(self, "lineout"):
-                self.computespectrum()
+            # if hasattr(self, "tth") and \
+            # hasattr(self, "dsp") and \
+            # hasattr(self, "lineout"):
+            #     self.computespectrum()
         else:
             msg = "only Parameters class permitted"
             raise ValueError(msg)
@@ -757,8 +1022,8 @@ class LeBaillight:
             msg = f"only masked arrays input is allowed."
             raise ValueError(msg)
 
-        if hasattr(self, "tth"):
-            self.computespectrum()
+        # if hasattr(self, "tth"):
+        #     self.computespectrum()
 
     @property
     def mask(self):
@@ -778,8 +1043,8 @@ class LeBaillight:
     def tth(self, val):
         if isinstance(val, dict):
             self._tth = val
-            if hasattr(self,"dsp"):
-                self.computespectrum()
+            # if hasattr(self,"dsp"):
+            #     self.computespectrum()
         else:
             msg = (f"two theta vallues need "
                    f"to be in a dictionary")
@@ -792,10 +1057,10 @@ class LeBaillight:
     def hkls(self, val):
         if isinstance(val, dict):
             self._hkls = val
-            if hasattr(self,"tth") and\
-            hasattr(self,"dsp") and\
-            hasattr(self,"lineout"):
-                self.computespectrum()
+            # if hasattr(self,"tth") and\
+            # hasattr(self,"dsp") and\
+            # hasattr(self,"lineout"):
+            #     self.computespectrum()
         else:
             msg = (f"two theta vallues need "
                    f"to be in a dictionary")
@@ -805,12 +1070,11 @@ class LeBaillight:
     def dsp(self):
         return self._dsp
     
-
     @dsp.setter
     def dsp(self, val):
         if isinstance(val, dict):
             self._dsp = val
-            self.computespectrum()
+            # self.computespectrum()
         else:
             msg = (f"two theta vallues need "
                    f"to be in a dictionary")
