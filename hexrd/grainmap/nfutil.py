f932aa8718d43a09783a9284f7275324ea552a14
darrencpagan <darren.c.pagan@gmail.com>
2022-02-23 08:59:28 -0500

    Update nfutil.py
    Update output_grain_map to take volume offsets and combines functionality to output as npz, hdf5, or vtk.

--- hexrd/grainmap/nfutil.py
+++ hexrd/grainmap/nfutil.py
@@ -1570,6 +1570,158 @@ def build_controller(check=None,generate=None,ncpus=2,chunk_size=10,limit=None):
 
     return controller
 
+def output_grain_map(data_location,data_stems,output_stem,vol_spacing,top_down=True,save_type=['npz']):
+
+    num_scans=len(data_stems)
+    
+    confidence_maps=[None]*num_scans
+    grain_maps=[None]*num_scans
+    Xss=[None]*num_scans
+    Yss=[None]*num_scans
+    Zss=[None]*num_scans
+    
+    if len(vol_spacing)==1:
+        vol_shifts=np.arange(0,vol_spacing[0]*num_scans+1e-12,vol_spacing[0])
+    else:
+        vol_shifts=vol_spacing
+    
+    
+    for ii in np.arange(num_scans):
+        print('Loading Volume %d ....'%(ii))
+        conf_data=np.load(os.path.join(data_location,data_stems[ii]+'_grain_map_data.npz'))
+        
+        confidence_maps[ii]=conf_data['confidence_map']
+        grain_maps[ii]=conf_data['grain_map']
+        Xss[ii]=conf_data['Xs']
+        Yss[ii]=conf_data['Ys']
+        Zss[ii]=conf_data['Zs']
+         
+    #assumes all volumes to be the same size
+    num_layers=grain_maps[0].shape[0]
+    
+    total_layers=num_layers*num_scans
+    
+    num_rows=grain_maps[0].shape[1]
+    num_cols=grain_maps[0].shape[2]
+    
+    grain_map_stitched=np.zeros((total_layers,num_rows,num_cols))
+    confidence_stitched=np.zeros((total_layers,num_rows,num_cols))
+    Xs_stitched=np.zeros((total_layers,num_rows,num_cols))
+    Ys_stitched=np.zeros((total_layers,num_rows,num_cols))
+    Zs_stitched=np.zeros((total_layers,num_rows,num_cols))
+    
+    
+    for ii in np.arange(num_scans):
+        if top_down==True:
+            grain_map_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=grain_maps[num_scans-1-ii]
+            confidence_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=confidence_maps[num_scans-1-ii]
+            Xs_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=\
+                Xss[num_scans-1-ii]
+            Zs_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=\
+                Zss[num_scans-1-ii]
+            Ys_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=Yss[num_scans-1-ii]+vol_shifts[ii]    
+        else:
+            
+            grain_map_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=grain_maps[ii]
+            confidence_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=confidence_maps[ii]
+            Xs_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=Xss[ii]
+            Zs_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=Zss[ii]
+            Ys_stitched[((ii)*num_layers):((ii)*num_layers+num_layers),:,:]=Yss[ii]+vol_shifts[ii]
+    
+    for ii in np.arange(len(save_type)):
+    
+        if save_type[ii] == 'hdf5':
+            
+            print('Writing HDF5 data...')
+            
+            hf = h5py.File(output_stem + '_assembled.h5', 'w')
+            hf.create_dataset('grain_map', data=grain_map_stitched)
+            hf.create_dataset('confidence', data=confidence_stitched)
+            hf.create_dataset('Xs', data=Xs_stitched)
+            hf.create_dataset('Ys', data=Ys_stitched)
+            hf.create_dataset('Zs', data=Zs_stitched)
+    
+        elif save_type[ii]=='npz':
+            
+            print('Writing NPZ data...')
+
+            np.savez(output_stem + '_assembled.npz',\
+             grain_map=grain_map_stitched,confidence=confidence_stitched,
+             Xs=Xs_stitched,Ys=Ys_stitched,Zs=Zs_stitched)
+            
+        elif save_type[ii]=='vtk':
+            
+            
+            print('Writing VTK data...')
+            # VTK Dump 
+            Xslist=Xs_stitched[:,:,:].ravel()
+            Yslist=Ys_stitched[:,:,:].ravel()
+            Zslist=Zs_stitched[:,:,:].ravel()
+            
+            grainlist=grain_map_stitched[:,:,:].ravel()
+            conflist=confidence_stitched[:,:,:].ravel()
+            
+            num_pts=Xslist.shape[0]
+            num_cells=(total_layers-1)*(num_rows-1)*(num_cols-1)
+            
+            f = open(os.path.join(output_stem +'_assembled.vtk'), 'w')
+            
+            
+            f.write('# vtk DataFile Version 3.0\n')
+            f.write('grainmap Data\n')
+            f.write('ASCII\n')
+            f.write('DATASET UNSTRUCTURED_GRID\n')
+            f.write('POINTS %d double\n' % (num_pts))
+            
+            for i in np.arange(num_pts):
+                f.write('%e %e %e \n' %(Xslist[i],Yslist[i],Zslist[i]))
+            
+            scale2=num_cols*num_rows
+            scale1=num_cols    
+                
+            f.write('CELLS %d %d\n' % (num_cells, 9*num_cells))   
+            for k in np.arange(Xs_stitched.shape[0]-1):
+                for j in np.arange(Xs_stitched.shape[1]-1):
+                    for i in np.arange(Xs_stitched.shape[2]-1):
+                        base=scale2*k+scale1*j+i    
+                        p1=base
+                        p2=base+1
+                        p3=base+1+scale1
+                        p4=base+scale1
+                        p5=base+scale2
+                        p6=base+scale2+1
+                        p7=base+scale2+scale1+1
+                        p8=base+scale2+scale1
+                        
+                        f.write('8 %d %d %d %d %d %d %d %d \n' \
+                                %(p1,p2,p3,p4,p5,p6,p7,p8))    
+                
+                
+            f.write('CELL_TYPES %d \n' % (num_cells))    
+            for i in np.arange(num_cells):
+                f.write('12 \n')    
+            
+            f.write('POINT_DATA %d \n' % (num_pts))
+            f.write('SCALARS grain_id int \n')  
+            f.write('LOOKUP_TABLE default \n')      
+            for i in np.arange(num_pts):
+                f.write('%d \n' %(grainlist[i]))    
+            
+            f.write('FIELD FieldData 1 \n' )
+            f.write('confidence 1 %d float \n' % (num_pts))       
+            for i in np.arange(num_pts):
+                f.write('%e \n' %(conflist[i]))  
+                
+            
+            f.close()
+            
+        else:
+            print('Not a valid save option, npz, vtk, or hdf5 allowed.')
+    
+    return grain_map_stitched, confidence_stitched, Xs_stitched, Ys_stitched, \
+            Zs_stitched
+
+
 
 # # assume that if os has fork, it will be used by multiprocessing.
 # # note that on python > 3.4 we could use multiprocessing get_start_method and
