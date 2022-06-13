f2b19db73c30867cf212908329b00af3d89b88e8
Chris Harris <chris.harris@kitware.com>
2022-06-13 09:46:47 -0400

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -87,7 +87,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
 
     ],
     entry_points = entry_points,
