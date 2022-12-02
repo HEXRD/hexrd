48712aa26af7ed5207c5321bc6b9ddeb69eb456e
Saransh Singh <saransh13@gmail.com>
2022-12-02 15:34:50 -0800

    skeleton of rfz module.

--- hexrd/sampleOrientations/rfz.py
+++ hexrd/sampleOrientations/rfz.py
@@ -9,5 +9,37 @@ else:
     prange = range
 
 @numba_njit_if_available(cache=True, nogil=True)
-def insideFZ(ro, pgnum):
+def getFZtypeandOrder(pgnum):
+    FZtype = FZtypeArray[pgnum-1]
+    FZorder = FZorderArray[pgnum-1]
+    return np.array([FZtype, FZorder])
+
+@numba_njit_if_available(cache=True, nogil=True)
+def insideCyclicFZ(ro, FZtype, FZorder):
     return True
+
+@numba_njit_if_available(cache=True, nogil=True)
+def insideDihedralFZ(ro, FZorder):
+    return True
+
+@numba_njit_if_available(cache=True, nogil=True)
+def insideCubicFZ(ro, kwrd):
+    return True
+
+@numba_njit_if_available(cache=True, nogil=True)
+def insideFZ(ro, pgnum):
+    res = getFZtypeandOrder(pgnum)
+    FZtype = res[0] 
+    FZorder = res[1]
+
+    if FZtype == 0:
+        return True
+    elif FZtype == 1:
+        return insideCyclicFZ(ro, FZtype, FZorder)
+    elif FZtype == 2:
+        return insideDihedralFZ(ro, FZorder)
+    elif FZtype == 3:
+        return insideCubicFZ(ro, 'tet')
+    elif FZtype == 4:
+        return insideCubicFZ(ro, 'oct')
+
