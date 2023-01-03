f77762173189a09ba691f2215d76c0b6b3023161
Saransh Singh <saransh13@gmail.com>
2023-01-03 12:47:56 -0800

    All code written. Needsa to be tested against sampleRFZ package now.

--- hexrd/sampleOrientations/rfz.py
+++ hexrd/sampleOrientations/rfz.py
@@ -34,11 +34,62 @@ def insideCyclicFZ(ro, FZorder):
 
 @numba_njit_if_available(cache=True, nogil=True)
 def insideDihedralFZ(ro, FZorder):
-    return True
+    if np.abs(ro[3]) >= np.sqrt(3.0):
+        return False
+    else:
+        rod = ro[0:3] * ro[3]
+
+    c1 = (rod[2] <= constants.BP[FZorder-1])
+
+    if c1:
+        if   FZorder == 2:
+            c2 = np.logical_and(np.abs(rod[0]) <= 1.0, 
+                                np.abs(rod[1]) <= 1.0)
+
+        elif FZorder == 3:
+            srt = np.sqrt(3.0)/2.0
+            c2 = np.abs(srt*rod[0] + 0.5*rod[1]) <= 1.0
+            c3 = np.abs(srt*rod[0] - 0.5*rod[1]) <= 1.0
+            c4 = np.abs(rod[1]) <= 1.0
+            return np.logical_and(c2, 
+                   np.logical_and(c3, c4))
+
+        elif FZorder == 4:
+            r22 = 1.0/np.sqrt(2.0)
+            c2 = np.logical_and(np.abs(rod[0]) <= 1.0,
+                                np.abs(rod[1]) <= 1.0)
+            c3 = np.logical_and(r22*np.abs(rod[0]+rod[1]) <= 1.0,
+                                r22*np.abs(rod[0]-rod[1]) <= 1.0)
+            return np.logical_and(c2, c3)
+
+        elif FZorder == 6:
+            srt = np.sqrt(3.0)/2.0
+            c2 = np.abs(0.5*rod[0] + srt*rod[1]) < 1.0
+            c2 = np.logical_and(c2,
+                 np.abs(srt*rod[0] + 0.5*rod[1]) < 1.0)
+            c2 = np.logical_and(c2,
+                 np.abs(0.5*rod[0] - srt*rod[1]) < 1.0)
+            c2 = np.logical_and(c2,
+                 np.abs(srt*rod[0] - 0.5*rod[1]) < 1.0)
+            c2 = np.logical_and(c2, 
+                 np.logical_and(np.abs(rod[0]) <= 1.0,
+                                np.abs(rod[1]) <= 1.0))
+            return c2
+    else:
+        return False
 
 @numba_njit_if_available(cache=True, nogil=True)
 def insideCubicFZ(ro, kwrd):
-    return True
+    rod = np.abs(ro[0:3] * ro[3])
+
+    if kwrd == 'oct':
+        c1 = (np.max(rod) - constants.BP[3]) <= 1E-8
+    else:
+        c1 = True
+
+    c2 = (rod[0]+rod[1]+rod[2] - 1.0) <= 1E-8
+    res = np.logical_and(c1, c2)
+    return res
 
 @numba_njit_if_available(cache=True, nogil=True)
 def insideFZ(ro, pgnum):
