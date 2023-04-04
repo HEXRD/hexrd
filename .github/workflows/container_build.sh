0faa0bd16507373fca7bdeb014e949ebb7e4b6af
Patrick Avery <patrick.avery@kitware.com>
2023-04-04 16:14:09 -0500

    Update gcc version to 10
    Newer gcc versions compile code that runs faster. And we want to allow c99 for loops
    by default (this will fix a current error).
    
    gcc 10 is what is used by manylinux2014, so it has a good basis for being used.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- .github/workflows/container_build.sh
+++ .github/workflows/container_build.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 cd /github/workspace/
-yum install -y wget git gcc
+
+# Install dependencies
+yum install -y wget git centos-release-scl
+
+# Need to install packages that depend on centos-release-scl on a different line.
+# This will use gcc==10, which is the same as what manylinux2014 uses.
+yum install -y devtoolset-10
+
+# Enable this toolset
+source /opt/rh/devtoolset-10/enable
+
+# Download and install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
+
+# Set up the hexrd channel
 ${HOME}/miniconda3/bin/conda create --override-channels -c conda-forge -y -n hexrd python=3.9
 ${HOME}/miniconda3/bin/activate hexrd
 ${HOME}/miniconda3/bin/conda activate hexrd
+
+# Install conda build and create output directory
 ${HOME}/miniconda3/bin/conda install --override-channels -c conda-forge conda-build -y
 mkdir output
+
+# Build the package
 ${HOME}/miniconda3/bin/conda build --override-channels -c conda-forge --output-folder output/ conda.recipe/
