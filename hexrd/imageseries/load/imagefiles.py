cd7f2ce1feb9da4d44ea8cf9b7dc9a494d6670e2
Brianna Major <brianna.major@kitware.com>
2023-05-10 11:39:19 -0400

    Rename SCALE_FACTOR -> GEL_SCALE_FACTOR
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- hexrd/imageseries/load/imagefiles.py
+++ hexrd/imageseries/load/imagefiles.py
@@ -19,7 +19,7 @@ from . import ImageSeriesAdapter
 from .metadata import yamlmeta
 from ..imageseriesiter import ImageSeriesIterator
 
-SCALE_FACTOR = 2.9452155399372724e-07
+GEL_SCALE_FACTOR = 2.9452155399372724e-07
 
 
 class ImageFilesImageSeriesAdapter(ImageSeriesAdapter):
@@ -68,7 +68,7 @@ class ImageFilesImageSeriesAdapter(ImageSeriesAdapter):
                 raise RuntimeError("specified dtype will truncate image")
             return np.array(data, dtype=self._dtype)
         elif file_extension == '.gel':
-            return data.astype(np.float64)**2 * SCALE_FACTOR
+            return data.astype(np.float64)**2 * GEL_SCALE_FACTOR
         else:
             return data
 
@@ -226,7 +226,7 @@ class FileInfo(object):
             self.dat = img.data
             if os.path.splitext(filename)[-1] == '.gel':
                 raw_img = np.invert(img.data)
-                self.dat = raw_img.astype(np.float64)**2 * SCALE_FACTOR
+                self.dat = raw_img.astype(np.float64)**2 * GEL_SCALE_FACTOR
 
         d = kwargs.copy()
         self._empty = d.pop('empty', 0)
