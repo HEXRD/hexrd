efa21938aed36ba9aae87620cffcf6c7dfa3950d
Zack Singer <jordan.singer@udri.udayton.edu>
2023-07-13 09:15:44 -0400

    Dynamically allow compiler flags for Windows/Linux

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages, Extension
 from pathlib import Path
 import numpy
 np_include_dir = numpy.get_include()
+import distutils.ccompiler
 
 install_reqs = [
     'fabio>=0.11',
@@ -21,6 +22,15 @@ install_reqs = [
     'tqdm',
     'xxhash',
 ]
+
+# This will determine which compiler is being used to build the C modules
+compiler_type = distutils.ccompiler.get_default_compiler()
+if compiler_type in ("unix", "mingw32"):
+    compiler_optimize_flags = ['-O3', '-ftree-vectorize']
+elif compiler_type == "msvc":
+    compiler_optimize_flags = ['/O2', '/Ob2', '/Oi', '/Ot', '/Oy', '/GL']
+else:
+    compiler_optimize_flags = []
 
 # This a hack to get around the fact that scikit-image on conda-forge doesn't install
 # dist info so setuptools can't find it, even though its there, which results in
@@ -41,6 +51,7 @@ def get_convolution_extensions():
     extra_compile_args=['-UNDEBUG']
     if not sys.platform.startswith('win'):
         extra_compile_args.append('-fPIC')
+    extra_compile_args += compiler_optimize_flags
     # Add '-Rpass-missed=.*' to ``extra_compile_args`` when compiling with clang
     # to report missed optimizations
     _convolve_ext = Extension(name='hexrd.convolution._convolve', sources=src_files,
@@ -59,7 +70,7 @@ def get_old_xfcapi_extension_modules():
         'hexrd.extensions._transforms_CAPI',
         sources=srclist,
         include_dirs=[np_include_dir],
-        extra_compile_args=['-O3', '-ftree-vectorize'],
+        extra_compile_args=compiler_optimize_flags,
     )
 
     return [transforms_mod]
@@ -70,7 +81,7 @@ def get_new_xfcapi_extension_modules():
         'hexrd.extensions._new_transforms_capi',
         sources=['hexrd/transforms/new_capi/module.c'],
         include_dirs=[np_include_dir],
-        extra_compile_args=['-O3', '-ftree-vectorize'],
+        extra_compile_args=compiler_optimize_flags,
     )
 
     return [transforms_mod]
