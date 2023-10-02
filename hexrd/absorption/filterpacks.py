7785d0c36f4e087e7378d94aeafe7853e9b3c9b7
Saransh Singh <saransh13@gmail.com>
2023-10-02 11:29:41 -0700

    define some properties.

--- hexrd/absorption/filterpacks.py
+++ hexrd/absorption/filterpacks.py
@@ -6,4 +6,21 @@ class filterpack():
     related to absorption by the filter pack.
     """
     def __init__(self,
-                 material)
+                 filtermaterial=None,
+                 thickness=10):
+    if isinstance(filtermaterial, Material):
+        self.filtermaterial = filtermaterial
+
+
+    @property
+    def absorption_length(self):
+        if hasattr(self, material):
+            self.material.absorption_length
+        else:
+            return self._absorption_length
+
+    @absorption_length.setter
+    def absorption_length(self, val):
+        """absorption length in microns
+        """
+        self._absorption_length = val
