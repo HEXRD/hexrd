ab3749f14c4f3b79c208e2839a676bc4a0281638
Patrick Avery <patrick.avery@kitware.com>
2024-01-31 12:56:05 -0600

    Add single rmat multiple gvec, and rotation test
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ tests/common.py
@@ -0,0 +1,42 @@
+import math
+
+import numpy as np
+
+import hexrd.constants as ct
+
+
+def convert_axis_angle_to_rmat(axis, angle):
+    # Copied from: https://github.com/ovillellas/xrd-transforms/blob/b94f8b2d7839d883829d00a2adc5bec9c80e0116/test_xrd_transforms/common.py#L59  # noqa
+    # This is based on
+    # https://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToMatrix/
+
+    angle = float(angle)
+    axis = np.array(axis, dtype=float)
+    assert axis.shape == (3,)
+
+    if abs(angle) < ct.epsf:
+        return ct.identity_3x3
+
+    axis_norm = np.linalg.norm(axis)
+    if axis_norm < ct.epsf:
+        raise ValueError("axis is zero")
+
+    axis /= axis_norm
+
+    m = np.empty((3, 3), dtype=float)
+
+    c = math.cos(angle)
+    s = math.sin(angle)
+    t = 1.0 - c
+
+    m[0, 0] = c + axis[0]*axis[0]*t
+    m[0, 1] = axis[0]*axis[1]*t - axis[2]*s
+    m[0, 2] = axis[0]*axis[2]*t + axis[1]*s
+    m[1, 0] = axis[0]*axis[1]*t + axis[2]*s
+    m[1, 1] = c + axis[1]*axis[1]*t
+    m[1, 2] = axis[1]*axis[2]*t - axis[0]*s
+    m[2, 0] = axis[0]*axis[2]*t - axis[1]*s
+    m[2, 1] = axis[1]*axis[2]*t + axis[0]*s
+    m[2, 2] = c + axis[2]*axis[2]*t
+
+    return m
