853afd6e25b78809ea5fb15044366e24c10a941d
Saransh Singh <saransh13@gmail.com>
2024-02-29 16:00:32 -0800

    added some description

--- hexrd/absorption/layeredsamples.py
+++ hexrd/absorption/layeredsamples.py
@@ -7,4 +7,16 @@ class layeredsamples():
     package. the class uses information from a material
     and instrument class to determine these corrections.
 
+    we will only consider absorption after the
+    diffraction has occured i.e. absorption by the 
+    sample and the window only. absorption by the 
+    filterpacks etc. will be in a separate class
+
+    The most important equation to use will be eqn. 42
+    from Rygg et al., X-ray diffraction at the National 
+    Ignition Facility, Rev. Sci. Instrum. 91, 043902 (2020)
     """
+    def __init__(
+                self,
+                sample=None,
+                window=None):
