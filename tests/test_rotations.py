0c645f8a46853dbb434b2a6254e9e114f49b9955
donald e. boyce <donald.e.boyce@gmail.com>
2024-05-27 11:57:46 -0400

    improved test_rotations to use every Laue group for misorientation testing

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -7,11 +7,20 @@ from hexrd import rotations
 
 
 def test_misorientations():
-    qsym = symmetry.quatOfLaueGroup('oh')
-    q1 = qsym[:, 3:4]
-    q2 = qsym[:, (6, 7, 18)]
-    ang, mis = rotations.misorientation(q1, q2, (qsym,))
-    print("ang: ", ang, "\nmis: ", mis)
-    assert np.allclose(ang, 0.0)
-    assert np.allclose(mis[0, :], 1.)
-    assert np.allclose(mis[1:, :], 0.)
+    """Use Laue groups to test for zero misorientation"""
+    #
+    # This tests that all the 11 Laue groups have zero misorientation with
+    # their own members.
+    #
+    laue_groups = [
+        "ci", "c2h", "d2h", "c4h", "d4h", "c3i",
+        "d3d", "c6h", "d6h", "th", "oh"
+    ]
+    for lg in laue_groups:
+        print("group: ", lg)
+        qsym = symmetry.quatOfLaueGroup(lg)
+        q1 = qsym[:, -1:]
+        ang, mis = rotations.misorientation(q1, qsym, (qsym,))
+        assert np.allclose(ang, 0.0)
+        assert np.allclose(mis[0, :], 1.)
+        assert np.allclose(mis[1:, :], 0.)
