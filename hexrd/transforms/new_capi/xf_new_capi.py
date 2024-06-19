441912346cbea972f86b3f979d412befd7e4e2a8
Kevin Lewis <kevindlewis23@gmail.com>
2024-06-19 13:04:49 -0400

    Ruler compliance, naming bugfix

--- hexrd/transforms/new_capi/xf_new_capi.py
+++ hexrd/transforms/new_capi/xf_new_capi.py
@@ -17,7 +17,8 @@ Currently, this implementation contains code for the following functions:
  - rotate_vecs_about_axis
  - quat_distance
 
-There are also some functions that maybe would be needed in the transforms module:
+There are also some functions that maybe would be needed in the transforms 
+module:
  - makeGVector
  - makeRotMatOfQuat
  - homochoricOfQuat
@@ -29,14 +30,10 @@ from hexrd.extensions import _new_transforms_capi as _impl
 import numpy as np
 
 
-
 @xf_api
-def angles_to_gvec(
-        angs,
-        beam_vec=None, eta_vec=None,
-        chi=None, rmat_c=None):
+def angles_to_gvec(angs, beam_vec=None, eta_vec=None, chi=None, rmat_c=None):
     """
-    
+
     Takes triplets of angles in the beam frame (2*theta, eta, omega)
     to components of unit G-vectors in the LAB frame.  If the omega
     values are not trivial (i.e. angs[:, 2] = 0.), then the components
@@ -48,20 +45,24 @@ def angles_to_gvec(
     Parameters
     ----------
     angs : ndarray
-        The euler angles of diffraction. The last dimension must be 3.  In (2*theta, eta, omega) format.
+        The euler angles of diffraction. The last dimension must be 3.  In 
+        (2*theta, eta, omega) format.
     beam_vec : ndarray, optional
-        Unit vector pointing towards the X-ray source in the lab frame.  Defaults to [0,0,-1]
+        Unit vector pointing towards the X-ray source in the lab frame.  
+        Defaults to [0,0,-1]
     eta_vec : ndarray, optional
         Vector defining eta=0 in the lab frame.  Defaults to [1,0,0]
     chi : float, optional
         The inclination angle of the sample frame about the lab frame X.
     rmat_c : ndarray, optional
-        The change of basis matrix from the reciprocal frame to the crystal frame.  Defaults to the identity.
+        The change of basis matrix from the reciprocal frame to the crystal 
+        frame.  Defaults to the identity.
 
     Returns
     -------
     ndarray
-        (3,n) array of unit reciprocal lattice vectors, frame depends on the input parameters
+        (3,n) array of unit reciprocal lattice vectors, frame depends on the 
+        input parameters
     """
 
     orig_ndim = angs.ndim
@@ -69,15 +70,17 @@ def angles_to_gvec(
     # if only a pair is provided... convert to a triplet with omegas == 0
     # so that behavior is preserved.
     if angs.shape[-1] == 2:
-        angs = np.hstack((angs, np.zeros(angs.shape[:-1]+(1,))))
+        angs = np.hstack((angs, np.zeros(angs.shape[:-1] + (1,))))
 
-    angs = np.ascontiguousarray( np.atleast_2d(angs) )
+    angs = np.ascontiguousarray(np.atleast_2d(angs))
     beam_vec = beam_vec if beam_vec is not None else cnst.beam_vec
-    beam_vec = np.ascontiguousarray( beam_vec.flatten() )
+    beam_vec = np.ascontiguousarray(beam_vec.flatten())
     eta_vec = eta_vec if eta_vec is not None else cnst.eta_vec
-    eta_vec = np.ascontiguousarray( eta_vec.flatten() )
+    eta_vec = np.ascontiguousarray(eta_vec.flatten())
     chi = 0.0 if chi is None else float(chi)
-    rmat_c = cnst.identity_3x3 if rmat_c is None else np.ascontiguousarray( rmat_c )
+    rmat_c = (
+        cnst.identity_3x3 if rmat_c is None else np.ascontiguousarray(rmat_c)
+    )
 
     result = _impl.anglesToGVec(angs, beam_vec, eta_vec, chi, rmat_c)
 
@@ -85,12 +88,9 @@ def angles_to_gvec(
 
 
 @xf_api
-def angles_to_dvec(
-        angs,
-        beam_vec=None, eta_vec=None,
-        chi=None, rmat_c=None):
+def angles_to_dvec(angs, beam_vec=None, eta_vec=None, chi=None, rmat_c=None):
     """
-    
+
     Takes triplets of angles in the beam frame (2*theta, eta, omega)
     to components of unit diffraction vectors in the LAB frame.  If the omega
     values are not trivial (i.e. angs[:, 2] = 0.), then the components
@@ -101,38 +101,46 @@ def angles_to_dvec(
     Parameters
     ----------
     angs : ndarray
-        The euler angles of diffraction. The last dimension must be 3.  In (2*theta, eta, omega) format.
+        The euler angles of diffraction. The last dimension must be 3.  In 
+        (2*theta, eta, omega) format.
     beam_vec : ndarray, optional
-        Unit vector pointing towards the X-ray source in the lab frame.  Defaults to [0,0,-1]
+        Unit vector pointing towards the X-ray source in the lab frame.  
+        Defaults to [0,0,-1]
     eta_vec : ndarray, optional
         Vector defining eta=0 in the lab frame.  Defaults to [1,0,0]
     chi : float, optional
         The inclination angle of the sample frame about the lab frame X.
     rmat_c : ndarray, optional
-        The change of basis matrix from the reciprocal frame to the crystal frame.  Defaults to the identity.
+        The change of basis matrix from the reciprocal frame to the crystal 
+        frame.  Defaults to the identity.
 
     Returns
     -------
     ndarray
-        (3,n) array of unit diffraction vectors, frame depends on the input parameters
+        (3,n) array of unit diffraction vectors, frame depends on the input 
+        parameters
     """
     # TODO: Improve capi to avoid multiplications when rmat_c is None
     beam_vec = beam_vec if beam_vec is not None else cnst.beam_vec
     eta_vec = eta_vec if eta_vec is not None else cnst.eta_vec
 
-    angs = np.ascontiguousarray( np.atleast_2d(angs) )
-    beam_vec = np.ascontiguousarray( beam_vec.flatten() )
-    eta_vec = np.ascontiguousarray( eta_vec.flatten() )
-    rmat_c = np.ascontiguousarray(rmat_c) if rmat_c is not None else cnst.identity_3x3
+    angs = np.ascontiguousarray(np.atleast_2d(angs))
+    beam_vec = np.ascontiguousarray(beam_vec.flatten())
+    eta_vec = np.ascontiguousarray(eta_vec.flatten())
+    rmat_c = (
+        np.ascontiguousarray(rmat_c)
+        if rmat_c is not None
+        else cnst.identity_3x3
+    )
     chi = 0.0 if chi is None else float(chi)
 
-    return _impl.anglesToDVec(angs,
-                                         beam_vec, eta_vec,
-                                         chi, rmat_c)
+    return _impl.anglesToDVec(angs, beam_vec, eta_vec, chi, rmat_c)
+
 
 def makeGVector(hkl, bMat):
     """
-    Take a crystal relative b matrix onto a list of hkls to output unit reciprocal latice vectors (a.k.a. lattice plane normals)
+    Take a crystal relative b matrix onto a list of hkls to output unit 
+    reciprocal latice vectors (a.k.a. lattice plane normals)
 
 
     Parameters
@@ -140,12 +148,14 @@ def makeGVector(hkl, bMat):
     hkl : ndarray
         (3,n) ndarray of n hstacked reciprocal lattice vector component triplets
     bMat : ndarray
-        (3,3) ndarray of the change of basis matrix from the reciprocal lattice to the crystal reference frame
+        (3,3) ndarray of the change of basis matrix from the reciprocal 
+        lattice to the crystal reference frame
 
     Returns
     -------
     ndarray
-        (3,n) ndarray of n unit reciprocal lattice vectors (a.k.a. lattice plane normals)
+        (3,n) ndarray of n unit reciprocal lattice vectors (a.k.a. lattice plane
+        normals)
 
     """
     assert hkl.shape[0] == 3, 'hkl input must be (3, n)'
@@ -153,13 +163,18 @@ def makeGVector(hkl, bMat):
 
 
 @xf_api
-def gvec_to_xy(gvec_c,
-               rmat_d, rmat_s, rmat_c,
-               tvec_d, tvec_s, tvec_c,
-               beam_vec=None,
-               vmat_inv=None,
-               bmat=None):
-    
+def gvec_to_xy(
+    gvec_c,
+    rmat_d,
+    rmat_s,
+    rmat_c,
+    tvec_d,
+    tvec_s,
+    tvec_c,
+    beam_vec=None,
+    vmat_inv=None,
+    bmat=None,
+):
     """
     Takes a concatenated list of reciprocal lattice vectors components in the
     CRYSTAL FRAME to the specified detector-relative frame, subject to the
@@ -206,9 +221,9 @@ def gvec_to_xy(gvec_c,
     Returns
     -------
     array_like
-        The ([M, ][N, ] 2) array of [x, y] diffracted beam intersections for each
-        of the N input G-vectors in the DETECTOR FRAME (all Z_d coordinates are
-        0 and excluded) and for each of the M candidate positions. For each
+        The ([M, ][N, ] 2) array of [x, y] diffracted beam intersections for 
+        each of the N input G-vectors in the DETECTOR FRAME (all Z_d coordinates 
+        are 0 and excluded) and for each of the M candidate positions. For each
         input G-vector that cannot satisfy a Bragg condition or intersect the
         detector plane, [NaN, Nan] is returned.
 
@@ -223,36 +238,39 @@ def gvec_to_xy(gvec_c,
     beam_vec = beam_vec if beam_vec is not None else cnst.beam_vec
 
     orig_ndim = gvec_c.ndim
-    gvec_c  = np.ascontiguousarray( np.atleast_2d(gvec_c) )
-    rmat_s  = np.ascontiguousarray( rmat_s )
-    tvec_d  = np.ascontiguousarray( tvec_d.flatten()  )
-    tvec_s  = np.ascontiguousarray( tvec_s.flatten()  )
-    tvec_c  = np.ascontiguousarray( tvec_c.flatten()  )
-    beam_vec = np.ascontiguousarray( beam_vec.flatten() )
+    gvec_c = np.ascontiguousarray(np.atleast_2d(gvec_c))
+    rmat_s = np.ascontiguousarray(rmat_s)
+    tvec_d = np.ascontiguousarray(tvec_d.flatten())
+    tvec_s = np.ascontiguousarray(tvec_s.flatten())
+    tvec_c = np.ascontiguousarray(tvec_c.flatten())
+    beam_vec = np.ascontiguousarray(beam_vec.flatten())
 
     # depending on the number of dimensions of rmat_s use either the array
     # version or the "scalar" (over rmat_s) version. Note that rmat_s is either
     # a 3x3 matrix (ndim 2) or an nx3x4 array of matrices (ndim 3)
     if rmat_s.ndim > 2:
-        result =  _impl.gvecToDetectorXYArray(gvec_c,
-                                              rmat_d, rmat_s, rmat_c,
-                                              tvec_d, tvec_s, tvec_c,
-                                              beam_vec)
+        result = _impl.gvecToDetectorXYArray(
+            gvec_c, rmat_d, rmat_s, rmat_c, tvec_d, tvec_s, tvec_c, beam_vec
+        )
     else:
-        result =  _impl.gvecToDetectorXY(gvec_c,
-                                         rmat_d, rmat_s, rmat_c,
-                                         tvec_d, tvec_s, tvec_c,
-                                         beam_vec)
+        result = _impl.gvecToDetectorXY(
+            gvec_c, rmat_d, rmat_s, rmat_c, tvec_d, tvec_s, tvec_c, beam_vec
+        )
     return result[0] if orig_ndim == 1 else result
 
 
 @xf_api
-def xy_to_gvec(xy_d,
-               rmat_d, rmat_s,
-               tvec_d, tvec_s, tvec_c,
-               rmat_b=None,
-               distortion=None,
-               output_ref=False):
+def xy_to_gvec(
+    xy_d,
+    rmat_d,
+    rmat_s,
+    tvec_d,
+    tvec_s,
+    tvec_c,
+    rmat_b=None,
+    distortion=None,
+    output_ref=False,
+):
     """
     Takes a list cartesian (x, y) pairs in the DETECTOR FRAME and calculates
     the associated reciprocal lattice (G) vectors and (bragg angle, azimuth)
@@ -295,37 +313,45 @@ def xy_to_gvec(xy_d,
     array_like, optional
         if output_ref is True
     """
-    # TODO: in the C library beam vector and eta vector are expected. However we receive
-    # rmat_b. Please check this!
+    # TODO: in the C library beam vector and eta vector are expected. However 
+    # we receive rmat_b. Please check this!
     #
-    # It also seems that the output_ref version is not present as the argument gets
-    # ignored
+    # It also seems that the output_ref version is not present as the argument 
+    # gets ignored
 
     rmat_b = rmat_b if rmat_b is not None else cnst.identity_3x3
 
-    # the code seems to ignore this argument, assume output_ref == True not implemented
-    assert not output_ref , 'output_ref not implemented'
+    # the code seems to ignore this argument, assume output_ref == True not 
+    # implemented
+    assert not output_ref, 'output_ref not implemented'
 
     if distortion is not None:
         xy_d = distortion.unwarp(xy_d)
 
-    xy_d  = np.ascontiguousarray( np.atleast_2d(xy_d) )
-    rmat_d = np.ascontiguousarray( rmat_d )
-    rmat_s = np.ascontiguousarray( rmat_s )
-    tvec_d  = np.ascontiguousarray( tvec_d.flatten() )
-    tvec_s  = np.ascontiguousarray( tvec_s.flatten() )
-    tvec_c  = np.ascontiguousarray( tvec_c.flatten() )
-    beam_vec = np.ascontiguousarray( (-rmat_b[:,2]).flatten() )
-    eta_vec  = np.ascontiguousarray( rmat_b[:,0].flatten() ) #check this!
-    return _impl.detectorXYToGvec(xy_det,
-                                  rmat_d, rmat_s,
-                                  tvec_d, tvec_s, tvec_c,
-                                  beam_vec, eta_vec)
+    xy_d = np.ascontiguousarray(np.atleast_2d(xy_d))
+    rmat_d = np.ascontiguousarray(rmat_d)
+    rmat_s = np.ascontiguousarray(rmat_s)
+    tvec_d = np.ascontiguousarray(tvec_d.flatten())
+    tvec_s = np.ascontiguousarray(tvec_s.flatten())
+    tvec_c = np.ascontiguousarray(tvec_c.flatten())
+    beam_vec = np.ascontiguousarray((-rmat_b[:, 2]).flatten())
+    eta_vec = np.ascontiguousarray(rmat_b[:, 0].flatten())  # check this!
+    return _impl.detectorXYToGvec(
+        xy_d, rmat_d, rmat_s, tvec_d, tvec_s, tvec_c, beam_vec, eta_vec
+    )
 
 
-#@xf_api
-def oscillAnglesOfHKLs(hkls, chi, rMat_c, bMat, wavelength,
-                       vInv=None, beamVec=cnst.beam_vec, etaVec=cnst.eta_vec):
+# @xf_api
+def oscillAnglesOfHKLs(
+    hkls,
+    chi,
+    rMat_c,
+    bMat,
+    wavelength,
+    vInv=None,
+    beamVec=cnst.beam_vec,
+    etaVec=cnst.eta_vec,
+):
     # this was oscillAnglesOfHKLs
     hkls = np.array(hkls, dtype=float, order='C')
     if vInv is None:
@@ -333,11 +359,11 @@ def oscillAnglesOfHKLs(hkls, chi, rMat_c, bMat, wavelength,
     else:
         vInv = np.ascontiguousarray(vInv.flatten())
     beamVec = np.ascontiguousarray(beamVec.flatten())
-    etaVec  = np.ascontiguousarray(etaVec.flatten())
+    etaVec = np.ascontiguousarray(etaVec.flatten())
     bMat = np.ascontiguousarray(bMat)
     return _impl.oscillAnglesOfHKLs(
         hkls, chi, rMat_c, bMat, wavelength, vInv, beamVec, etaVec
-        )
+    )
 
 
 @xf_api
@@ -372,7 +398,7 @@ def unit_vector(vec_in):
         )
 
 
-#@xf_api
+# @xf_api
 def makeDetectorRotMat(tiltAngles):
     arg = np.ascontiguousarray(np.r_[tiltAngles].flatten())
     return _impl.makeDetectorRotMat(arg)
@@ -381,7 +407,8 @@ def makeDetectorRotMat(tiltAngles):
 # make_sample_rmat in CAPI is split between makeOscillRotMat
 # and makeOscillRotMatArray...
 
-#@xf_api
+
+# @xf_api
 def make_oscill_rot_mat(oscillAngles):
     chi, ome = oscillAngles
     ome = np.atleast_1d(ome)
@@ -389,7 +416,7 @@ def make_oscill_rot_mat(oscillAngles):
     return result.reshape((3, 3))
 
 
-#@xf_api
+# @xf_api
 def make_oscill_rot_mat_array(chi, omeArray):
     arg = np.ascontiguousarray(omeArray)
     return _impl.makeOscillRotMat(chi, arg)
@@ -397,9 +424,10 @@ def make_oscill_rot_mat_array(chi, omeArray):
 
 @xf_api
 def make_sample_rmat(chi, ome):
-    #TODO: Check this docstring
+    # TODO: Check this docstring
     """
-    Make a rotation matrix representing the COB from sample frame to the lab frame. 
+    Make a rotation matrix representing the COB from sample frame to the lab 
+    frame.
 
     Parameters
     ----------
@@ -423,9 +451,10 @@ def make_sample_rmat(chi, ome):
         # to call ascontiguousarray, but need to remove
         # the outer dimension from the result
         result = _impl.makeOscillRotMat(chi, ome_array)
-        result = result.reshape(3,3)
+        result = result.reshape(3, 3)
 
     return result
+
 
 @xf_api
 def make_rmat_of_expmap(exp_map):
@@ -436,7 +465,7 @@ def make_rmat_of_expmap(exp_map):
     ----------
     exp_map : array_like
         A 3-element sequence representing the exponential map n*phi.
-    
+
     Returns
     -------
     ndarray
@@ -448,8 +477,8 @@ def make_rmat_of_expmap(exp_map):
 
 @xf_api
 def make_binary_rmat(axis):
-    #TODO: Make this docstring.
-    
+    # TODO: Make this docstring.
+
     arg = np.ascontiguousarray(axis.flatten())
     return _impl.makeBinaryRotMat(arg)
 
@@ -463,7 +492,8 @@ def make_beam_rmat(bvec_l, evec_l):
     Parameters
     ----------
     bvec_l : ndarray
-        The beam vector in the lab frame, The (3, ) incident beam propagation vector components in the lab frame
+        The beam vector in the lab frame, The (3, ) incident beam propagation 
+        vector components in the lab frame.
         the default is [0, 0, -1], which is the standard setting.
     evec_l : ndarray
         Vector defining eta=0 in the lab frame.  Defaults to [1,0,0]
@@ -487,7 +517,8 @@ def validate_angle_ranges(ang_list, start_angs, stop_angs, ccw=True):
     stop_angs : ndarray
         The stopping angles
     ccw : bool, optional
-        If True, the angles are in the CCW range from start to stop.  Defaults to True.
+        If True, the angles are in the CCW range from start to stop.  
+        Defaults to True.
 
     Returns
     -------
@@ -527,10 +558,12 @@ def rotate_vecs_about_axis(angle, axis, vecs):
     result = _impl.rotate_vecs_about_axis(angle, axis, vecs)
     return result.T
 
+
 @xf_api
 def quat_distance(q1, q2, qsym):
     """
-    Distance between two quaternions, taking quaternions of symmetry into account.
+    Distance between two quaternions, taking quaternions of symmetry into 
+    account.
 
     Parameters
     ----------
@@ -540,7 +573,7 @@ def quat_distance(q1, q2, qsym):
         Second quaternion.
     qsym : ndarray
         List of symmetry quaternions.
-    
+
     Returns
     -------
     float
