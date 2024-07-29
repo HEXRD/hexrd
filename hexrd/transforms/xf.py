862a1059529c35e4ec89cba1988f229e06d67e51
Kevin Lewis <kevindlewis23@gmail.com>
2024-07-29 11:00:24 -0400

    PEP*

--- hexrd/transforms/xf.py
+++ hexrd/transforms/xf.py
@@ -38,7 +38,7 @@ from hexrd import matrixutil as mutil
 
 
 # Added to not break people importing these methods
-from hexrd.rotations import (mapAngle, 
+from hexrd.rotations import (mapAngle,
                              quatProductMatrix as quat_product_matrix,
                              arccosSafe, angularDifference)
 from hexrd.matrixutil import columnNorm, rowNorm
