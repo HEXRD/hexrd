524cd4b3e928ee2218f49ea20890ffa46dea186b
Kevin Lewis <kevindlewis23@gmail.com>
2024-08-09 13:32:57 -0400

    Starting to port over eigen transforms

---
+++ tests/transforms/test_make_binary_rmat.py
@@ -0,0 +1,25 @@
+# Generated random test cases from commented out code
+# Save the test cases to a file
+# Loading to test correctness
+
+
+from __future__ import absolute_import
+import numpy as np
+from hexrd.transforms.new_capi.xf_new_capi import make_binary_rmat
+from hexrd.rotations import quatOfAngleAxis, rotMatOfQuat
+from transforms.common import random_unit_vectors
+
+
+def test_make_binary_rmat():
+    np.random.seed(0)
+    for _ in range(100):
+        axis = random_unit_vectors()
+        rmat = make_binary_rmat(axis)
+
+        # Two binary rmats should be the identity
+        assert np.allclose(rmat @ rmat, np.eye(3))
+
+        rmat_expected = rotMatOfQuat(
+            quatOfAngleAxis(np.pi, np.c_[axis])
+        )
+        assert np.allclose(rmat, rmat_expected)
