f0f6c1b3da1ff7b08ab45390bd5f23a10df13e41
Saransh Singh <saransh13@gmail.com>
2024-08-20 11:46:44 -0700

    more PEP8

--- hexrd/material/symbols.py
+++ hexrd/material/symbols.py
@@ -1,6 +1,5 @@
 
 pstr_mkxtal = "\n\n    This is a program to create a HDF5 file for storing crystallographic information.\n "
-pstr_mkxtal = pstr_mkxtal + " This format is the same format as used in the EMsoft (electron microscoy) suite.\n "
 pstr_mkxtal = pstr_mkxtal + " The following inputs are required:\n "
 pstr_mkxtal = pstr_mkxtal + "         Crystal System:\n"
 pstr_mkxtal = pstr_mkxtal + "                 1. Cubic\n"
@@ -24,88 +23,98 @@ pstr_mkxtal = pstr_mkxtal + " a RHOMBOHEDRAL basis (second setting).  The defaul
 pstr_mkxtal = pstr_mkxtal + " trigonal symmetry is the hexagonal setting.  When you select\n"
 pstr_mkxtal = pstr_mkxtal + " crystal system 5 above, you will be prompted for the setting. \n"
 
-pstr_spacegroup = [ " P  1      " ," P -1      ", \
-# MONOCLINIC SPACE GROUPS
-" P 2       " ," P 21      " ," C 2       " ," P m       ", \
-" P c       " ," C m       " ," C c       " ," P 2/m     ", \
-" P 21/m    " ," C 2/m     " ," P 2/c     " ," P 21/c    ", \
-" C 2/c     ", \
-# ORTHORHOMBIC SPACE GROUPS
-" P 2 2 2   " ," P 2 2 21  " ," P 21 21 2 " ," P 21 21 21", \
-" C 2 2 21  " ," C 2 2 2   " ," F 2 2 2   " ," I 2 2 2   ", \
-" I 21 21 21" ," P m m 2   " ," P m c 21  " ," P c c 2   ", \
-" P m a 2   " ," P c a 21  " ," P n c 2   " ," P m n 21  ", \
-" P b a 2   " ," P n a 21  " ," P n n 2   " ," C m m 2   ", \
-" C m c 21  " ," C c c 2   " ," A m m 2   " ," A b m 2   ", \
-" A m a 2   " ," A b a 2   " ," F m m 2   " ," F d d 2   ", \
-" I m m 2   " ," I b a 2   " ," I m a 2   " ," P m m m   ", \
-" P n n n   " ," P c c m   " ," P b a n   " ," P m m a   ", \
-" P n n a   " ," P m n a   " ," P c c a   " ," P b a m   ", \
-" P c c n   " ," P b c m   " ," P n n m   " ," P m m n   ", \
-" P b c n   " ," P b c a   " ," P n m a   " ," C m c m   ", \
-" C m c a   " ," C m m m   " ," C c c m   " ," C m m a   ", \
-" C c c a   " ," F m m m   " ," F d d d   " ," I m m m   ", \
-" I b a m   " ," I b c a   " ," I m m a   ", \
-# TETRAGONAL SPACE GROUPS  
-" P 4       " ," P 41      " ," P 42      " ," P 43      ", \
-" I 4       " ," I 41      " ," P -4      " ," I -4      ", \
-" P 4/m     " ," P 42/m    " ," P 4/n     " ," P 42/n    ", \
-" I 4/m     " ," I 41/a    " ," P 4 2 2   " ," P 4 21 2  ", \
-" P 41 2 2  " ," P 41 21 2 " ," P 42 2 2  " ," P 42 21 2 ", \
-" P 43 2 2  " ," P 43 21 2 " ," I 4 2 2   " ," I 41 2 2  ", \
-" P 4 m m   " ," P 4 b m   " ," P 42 c m  " ," P 42 n m  ", \
-" P 4 c c   " ," P 4 n c   " ," P 42 m c  " ," P 42 b c  ", \
-" I 4 m m   " ," I 4 c m   " ," I 41 m d  " ," I 41 c d  ", \
-" P -4 2 m  " ," P -4 2 c  " ," P -4 21 m " ," P -4 21 c ", \
-" P -4 m 2  " ," P -4 c 2  " ," P -4 b 2  " ," P -4 n 2  ", \
-" I -4 m 2  " ," I -4 c 2  " ," I -4 2 m  " ," I -4 2 d  ", \
-" P 4/m m m " ," P 4/m c c " ," P 4/n b m " ," P 4/n n c ", \
-" P 4/m b m " ," P 4/m n c " ," P 4/n m m " ," P 4/n c c ", \
-" P 42/m m c" ," P 42/m c m" ," P 42/n b c" ," P 42/n n m", \
-" P 42/m b c" ," P 42/m n m" ," P 42/n m c" ," P 42/n c m", \
-" I 4/m m m " ," I 4/m c m " ," I 41/a m d" ," I 41/a c d", \
-# RHOMBOHEDRAL SPACE GROUPS  
-" P 3       " ," P 31      " ," P 32      " ," R 3       ", \
-" P -3      " ," R -3      " ," P 3 1 2   " ," P 3 2 1   ", \
-" P 31 1 2  " ," P 31 2 1  " ," P 32 1 2  " ," P 32 2 1  ", \
-" R 3 2     " ," P 3 m 1   " ," P 3 1 m   " ," P 3 c 1   ", \
-" P 3 1 c   " ," R 3 m     " ," R 3 c     " ," P -3 1 m  ", \
-" P -3 1 c  " ," P -3 m 1  " ," P -3 c 1  " ," R -3 m    ", \
-" R -3 c    ", \
-# HEXAGONAL SPACE GROUPS   
-" P 6       " ," P 61      " ," P 65      " ," P 62      ", \
-" P 64      " ," P 63      " ," P -6      " ," P 6/m     ", \
-" P 63/m    " ," P 6 2 2   " ," P 61 2 2  " ," P 65 2 2  ", \
-" P 62 2 2  " ," P 64 2 2  " ," P 63 2 2  " ," P 6 m m   ", \
-" P 6 c c   " ," P 63 c m  " ," P 63 m c  " ," P -6 m 2  ", \
-" P -6 c 2  " ," P -6 2 m  " ," P -6 2 c  " ," P 6/m m m ", \
-" P 6/m c c " ," P 63/m c m" ," P 63/m m c", \
-#CUBIC SPACE GROUPS
-" P 2 3     " ," F 2 3     " ," I 2 3     " ," P 21 3    ", \
-" I 21 3    " ," P m 3     " ," P n 3     " ," F m 3     ", \
-" F d 3     " ," I m 3     " ," P a 3     " ," I a 3     ", \
-" P 4 3 2   " ," P 42 3 2  " ," F 4 3 2   " ," F 41 3 2  ", \
-" I 4 3 2   " ," P 43 3 2  " ," P 41 3 2  " ," I 41 3 2  ", \
-" P -4 3 m  " ," F -4 3 m  " ," I -4 3 m  " ," P -4 3 n  ", \
-" F -4 3 c  " ," I -4 3 d  " ," P m 3 m   " ," P n 3 n   ", \
-" P m 3 n   " ," P n 3 m   " ," F m 3 m   " ," F m 3 c   ", \
-" F d 3 m   " ," F d 3 c   " ," I m 3 m   " ," I a 3 d   ", \
-# TRIGONAL GROUPS RHOMBOHEDRAL SETTING
-" R 3   |146" ," R -3  |148" ," R 3 2 |155" ," R 3 m |160", \
-" R 3 c |161" ," R -3 m|166" ," R -3 c|167"]
+pstr_spacegroup =
+[
+    " P  1      ", " P -1      ", \
+    # MONOCLINIC SPACE GROUPS
+    " P 2       ", " P 21      ", " C 2       ", " P m       ", \
+    " P c       ", " C m       ", " C c       ", " P 2/m     ", \
+    " P 21/m    ", " C 2/m     ", " P 2/c     ", " P 21/c    ", \
+    " C 2/c     ", \
+    # ORTHORHOMBIC SPACE GROUPS
+    " P 2 2 2   ", " P 2 2 21  ", " P 21 21 2 ", " P 21 21 21", \
+    " C 2 2 21  ", " C 2 2 2   ", " F 2 2 2   ", " I 2 2 2   ", \
+    " I 21 21 21", " P m m 2   ", " P m c 21  ", " P c c 2   ", \
+    " P m a 2   ", " P c a 21  ", " P n c 2   ", " P m n 21  ", \
+    " P b a 2   ", " P n a 21  ", " P n n 2   ", " C m m 2   ", \
+    " C m c 21  ", " C c c 2   ", " A m m 2   ", " A b m 2   ", \
+    " A m a 2   ", " A b a 2   ", " F m m 2   ", " F d d 2   ", \
+    " I m m 2   ", " I b a 2   ", " I m a 2   ", " P m m m   ", \
+    " P n n n   ", " P c c m   ", " P b a n   ", " P m m a   ", \
+    " P n n a   ", " P m n a   ", " P c c a   ", " P b a m   ", \
+    " P c c n   ", " P b c m   ", " P n n m   ", " P m m n   ", \
+    " P b c n   ", " P b c a   ", " P n m a   ", " C m c m   ", \
+    " C m c a   ", " C m m m   ", " C c c m   ", " C m m a   ", \
+    " C c c a   ", " F m m m   ", " F d d d   ", " I m m m   ", \
+    " I b a m   ", " I b c a   ", " I m m a   ", \
+    # TETRAGONAL SPACE GROUPS
+    " P 4       ", " P 41      ", " P 42      ", " P 43      ", \
+    " I 4       ", " I 41      ", " P -4      ", " I -4      ", \
+    " P 4/m     ", " P 42/m    ", " P 4/n     ", " P 42/n    ", \
+    " I 4/m     ", " I 41/a    ", " P 4 2 2   ", " P 4 21 2  ", \
+    " P 41 2 2  ", " P 41 21 2 ", " P 42 2 2  ", " P 42 21 2 ", \
+    " P 43 2 2  ", " P 43 21 2 ", " I 4 2 2   ", " I 41 2 2  ", \
+    " P 4 m m   ", " P 4 b m   ", " P 42 c m  ", " P 42 n m  ", \
+    " P 4 c c   ", " P 4 n c   ", " P 42 m c  ", " P 42 b c  ", \
+    " I 4 m m   ", " I 4 c m   ", " I 41 m d  ", " I 41 c d  ", \
+    " P -4 2 m  ", " P -4 2 c  ", " P -4 21 m ", " P -4 21 c ", \
+    " P -4 m 2  ", " P -4 c 2  ", " P -4 b 2  ", " P -4 n 2  ", \
+    " I -4 m 2  ", " I -4 c 2  ", " I -4 2 m  ", " I -4 2 d  ", \
+    " P 4/m m m ", " P 4/m c c ", " P 4/n b m ", " P 4/n n c ", \
+    " P 4/m b m ", " P 4/m n c ", " P 4/n m m ", " P 4/n c c ", \
+    " P 42/m m c", " P 42/m c m", " P 42/n b c", " P 42/n n m", \
+    " P 42/m b c", " P 42/m n m", " P 42/n m c", " P 42/n c m", \
+    " I 4/m m m ", " I 4/m c m ", " I 41/a m d", " I 41/a c d", \
+    # RHOMBOHEDRAL SPACE GROUPS
+    " P 3       ", " P 31      ", " P 32      ", " R 3       ", \
+    " P -3      ", " R -3      ", " P 3 1 2   ", " P 3 2 1   ", \
+    " P 31 1 2  ", " P 31 2 1  ", " P 32 1 2  ", " P 32 2 1  ", \
+    " R 3 2     ", " P 3 m 1   ", " P 3 1 m   ", " P 3 c 1   ", \
+    " P 3 1 c   ", " R 3 m     ", " R 3 c     ", " P -3 1 m  ", \
+    " P -3 1 c  ", " P -3 m 1  ", " P -3 c 1  ", " R -3 m    ", \
+    " R -3 c    ", \
+    # HEXAGONAL SPACE GROUPS
+    " P 6       ", " P 61      ", " P 65      ", " P 62      ", \
+    " P 64      ", " P 63      ", " P -6      ", " P 6/m     ", \
+    " P 63/m    ", " P 6 2 2   ", " P 61 2 2  ", " P 65 2 2  ", \
+    " P 62 2 2  ", " P 64 2 2  ", " P 63 2 2  ", " P 6 m m   ", \
+    " P 6 c c   ", " P 63 c m  ", " P 63 m c  ", " P -6 m 2  ", \
+    " P -6 c 2  ", " P -6 2 m  ", " P -6 2 c  ", " P 6/m m m ", \
+    " P 6/m c c ", " P 63/m c m", " P 63/m m c", \
+    # CUBIC SPACE GROUPS
+    " P 2 3     ", " F 2 3     ", " I 2 3     ", " P 21 3    ", \
+    " I 21 3    ", " P m 3     ", " P n 3     ", " F m 3     ", \
+    " F d 3     ", " I m 3     ", " P a 3     ", " I a 3     ", \
+    " P 4 3 2   ", " P 42 3 2  ", " F 4 3 2   ", " F 41 3 2  ", \
+    " I 4 3 2   ", " P 43 3 2  ", " P 41 3 2  ", " I 41 3 2  ", \
+    " P -4 3 m  ", " F -4 3 m  ", " I -4 3 m  ", " P -4 3 n  ", \
+    " F -4 3 c  ", " I -4 3 d  ", " P m 3 m   ", " P n 3 n   ", \
+    " P m 3 n   ", " P n 3 m   ", " F m 3 m   ", " F m 3 c   ", \
+    " F d 3 m   ", " F d 3 c   ", " I m 3 m   ", " I a 3 d   ", \
+    # TRIGONAL GROUPS RHOMBOHEDRAL SETTING
+    " R 3   |146", " R -3  |148", " R 3 2 |155", " R 3 m |160", \
+    " R 3 c |161", " R -3 m|166", " R -3 c|167"
+]
 
-xtal_dict = {1:'cubic', 2:'tetragonal', 3:'orthorhombic', 4:'hexagonal', 5:'trigonal', 6:'monoclinic', 7:'triclinic'}
-xtal_sys_dict = {'cubic':1, 'tetragonal':2, 'orthorhombic':3, 'hexagonal':4, 'trigonal':5, 'monoclinic':6, 'triclinic':7}
+
+xtal_dict = {1: 'cubic', 2: 'tetragonal', 3: 'orthorhombic',
+             4: 'hexagonal', 5: 'trigonal', 6: 'monoclinic',
+             7: 'triclinic'}
+xtal_sys_dict = {'cubic': 1, 'tetragonal': 2, 'orthorhombic': 3,
+                 'hexagonal': 4, 'trigonal': 5, 'monoclinic': 6,
+                 'triclinic': 7}
 
 
-pstr_pointgroup = ['    1','   -1','    2','    m','  2/m','  222', \
-'  mm2','  mmm','    4','   -4','  4/m','  422', \
-'  4mm',' -42m','4/mmm','    3','   -3','   32', \
-'   3m','  -3m','    6','   -6','  6/m','  622', \
-'  6mm',' -6m2','6/mmm','   23','   m3','  432', \
-' -43m',' m-3m','  532','  822',' 1022',' 1222' ]
 
-TRIG = [146,148,155,160,161,166,167]
+pstr_pointgroup = ['    1', '   -1', '    2', '    m', '  2/m', '  222',
+                   '  mm2', '  mmm', '    4', '   -4', '  4/m', '  422',
+                   '  4mm', ' -42m', '4/mmm', '    3', '   -3', '   32',
+                   '   3m', '  -3m', '    6', '   -6', '  6/m', '  622',
+                   '  6mm', ' -6m2', '6/mmm', '   23', '   m3', '  432',
+                   ' -43m', ' m-3m', '  532', '  822', ' 1022', ' 1222']
+
+TRIG = [146, 148, 155, 160, 161, 166, 167]
+
 
 # symbols and Z for all elements
 pstr_Elements = ' ------------------------------------ Periodic Table of the Elements --------------------------------------' + "\n" \
@@ -126,66 +135,66 @@ site symmetries of the origin. There are 24 such space
 groups
 '''
 two_origin_choice = {
-48 : ['222', '-1'],
-50 : ['222/n', '-1'],
-59 : ['mm2/n', '-1'],
-68 : ['222', '-1'],
-70 : ['222', '-1'],
-85 : ['-4', '-1'],
-86 : ['-4', '-1'],
-88 : ['-4', '-1'],
-125: ['422', '2/m'],
-126: ['422/n', '-1'],
-129: ['-4m2', '2/m'],
-130: ['-4/ncn', '-1'],
-133: ['-4121/c', '-1'],
-134: ['-42m', '2/m'],
-137: ['-4m2/n', '-1'],
-138: ['-4cg', '2/m'],
-141: ['-4m2', '2/m'],
-142: ['-4c21', '-1'],
-201: ['23', '-3'],
-203: ['23', '-3'],
-222: ['432', '-3'],
-224: ['-43m', '-3m'],
-227: ['-43m', '-3m'],
-228: ['23', '-3'],
+    48: ['222', '-1'],
+    50: ['222/n', '-1'],
+    59: ['mm2/n', '-1'],
+    68: ['222', '-1'],
+    70: ['222', '-1'],
+    85: ['-4', '-1'],
+    86: ['-4', '-1'],
+    88: ['-4', '-1'],
+    125: ['422', '2/m'],
+    126: ['422/n', '-1'],
+    129: ['-4m2', '2/m'],
+    130: ['-4/ncn', '-1'],
+    133: ['-4121/c', '-1'],
+    134: ['-42m', '2/m'],
+    137: ['-4m2/n', '-1'],
+    138: ['-4cg', '2/m'],
+    141: ['-4m2', '2/m'],
+    142: ['-4c21', '-1'],
+    201: ['23', '-3'],
+    203: ['23', '-3'],
+    222: ['432', '-3'],
+    224: ['-43m', '-3m'],
+    227: ['-43m', '-3m'],
+    228: ['23', '-3'],
 }
 
 
 def PrintPossibleSG(xtal_sys):
-        if(xtal_sys == 1):
-                sgmax = 230
-                sgmin = 195
-        elif(xtal_sys == 2):
-                sgmax = 142
-                sgmin = 75
-        elif(xtal_sys == 3):
-                sgmax = 74
-                sgmin = 16
-        elif(xtal_sys == 4):
-                sgmax = 194
-                sgmin = 168
-        elif(xtal_sys == 5):
-                sgmax = 167
-                sgmin = 143
-        elif(xtal_sys == 6):
-                sgmax = 15
-                sgmin = 3
-        elif(xtal_sys == 7):
-                sgmax = 2
-                sgmin = 1
+    if(xtal_sys == 1):
+        sgmax = 230
+        sgmin = 195
+    elif(xtal_sys == 2):
+        sgmax = 142
+        sgmin = 75
+    elif(xtal_sys == 3):
+        sgmax = 74
+        sgmin = 16
+    elif(xtal_sys == 4):
+        sgmax = 194
+        sgmin = 168
+    elif(xtal_sys == 5):
+        sgmax = 167
+        sgmin = 143
+    elif(xtal_sys == 6):
+        sgmax = 15
+        sgmin = 3
+    elif(xtal_sys == 7):
+        sgmax = 2
+        sgmin = 1
 
-        for i in range(sgmin,sgmax+1):
-                j = i - sgmin + 1
-                pstr = str(i) + ":" + pstr_spacegroup[i-1] + "\t"
-                if(j % 4 == 0 or j == sgmax):
-                        print(pstr)
-                else:
-                        print(pstr, end='')
-        print("\n")
+    for i in range(sgmin, sgmax+1):
+        j = i - sgmin + 1
+        pstr = str(i) + ":" + pstr_spacegroup[i-1] + "\t"
+        if(j % 4 == 0 or j == sgmax):
+            print(pstr)
+        else:
+            print(pstr, end='')
+    print("\n")
 
-        return sgmin, sgmax
+    return sgmin, sgmax
 
 
 #
@@ -1257,6 +1266,7 @@ HM_STR = r"""
    230        I a -3 d
 """
 
+
 def _buildDict(hstr):
     """build the dictionaries from the notation string
 
@@ -1267,7 +1277,7 @@ def _buildDict(hstr):
     for the rhombohedral lattices so that they use the hexagonal
     convention.
     """
-    d  = dict()
+    d = dict()
     di = dict()
     hs = hstr.split('\n')
     for l in hs:
