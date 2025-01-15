ce8ddc4db845f70210f739a7488f55093ee700d3
Christos Tsolakis <christos.tsolakis@kitware.com>
2025-01-15 11:00:27 -0500

    preprocess: Use builtin safeloader since we do not have tuple retrictions anymore

--- hexrd/preprocess/yaml_internals.py
+++ hexrd/preprocess/yaml_internals.py
@@ -1,26 +1,5 @@
 import yaml
 
-# Create custom yaml loader and  dumper that know about all types in
-# ArgumentsClasses and can handle python tuples.
-
-
-# custom loader that can deserialize python tuples tagged as such while using
-# safeloader
-class HexrdPPScriptArgumentsSafeLoader(yaml.SafeLoader):
-    def __init__(self, args, **kwargs):
-        super().__init__(args, **kwargs)
-
-        def construct_python_tuple(self, node):
-            return tuple(self.construct_sequence(node))
-
-        tag = "tag:yaml.org,2002:python/tuple"
-        if tag not in HexrdPPScriptArgumentsSafeLoader.yaml_constructors:
-            HexrdPPScriptArgumentsSafeLoader.add_constructor(
-                "tag:yaml.org,2002:python/tuple",
-                construct_python_tuple,
-            )
-
-
 class HexrdPPScriptArgumentsDumper(yaml.Dumper):
     # skip aliases, this makes sure that no references are used when using
     # immutable objects. i.e. referring to the same tuple twice it will just
