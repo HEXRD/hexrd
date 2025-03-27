293e7d41c1cb792ab2e3c3de53b70fbc52ece394
Kevin Welsh <kevin.welsh@verdantevolution.com>
2025-03-27 18:29:10 -0500

    Separate Spot class into its own file

--- hexrd/spot_tracking/spot_finder.py
+++ hexrd/spot_tracking/spot_finder.py
@@ -1,49 +1,17 @@
+"""Spot finding
+
+NOTE: you must have OpenCV installed to import this file
+"""
+
 import logging
 
 import cv2
 import numpy as np
-from dataclasses import dataclass
 from scipy.ndimage import center_of_mass
 
+from .spot import Spot
+
 LOGGER = logging.getLogger(__name__)
-
-
-@dataclass
-class Spot:
-    i: float
-    '''The row pixel coordinate of the spot: img[i, j]'''
-    j: float
-    '''The column pixel coordinate of the spot: img[i, j]'''
-    w: int
-    '''The full width of the bounding box of the spot'''
-    bounding_box: tuple[int, int, int, int]
-    '''The bounding box of the spot: (rmin, cmin, rmax, cmax)'''
-    max: float
-    '''The maximum pixel value in the spot'''
-    sum: float
-    '''The sum of all pixel values in the spot'''
-
-    def __array__(self) -> np.ndarray:
-        return np.array([
-            self.i,
-            self.j,
-            self.w,
-            *self.bounding_box,
-            self.max,
-            self.sum,
-        ])
-
-    @classmethod
-    def from_array(cls, array: np.ndarray) -> 'Spot':
-        a = array
-        return Spot(
-            a[0],
-            a[1],
-            int(a[2]),
-            tuple(a[3:7].tolist()),
-            a[7],
-            a[8],
-        )
 
 
 def bbox2(img):
