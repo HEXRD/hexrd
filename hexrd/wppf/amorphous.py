4588473d393c62470c7b83787a9b6e5a60223f85
Saransh Singh <saransh13@gmail.com>
2025-06-10 15:09:03 -0700

    working model for split gaussian and experimental data

--- hexrd/wppf/amorphous.py
+++ hexrd/wppf/amorphous.py
@@ -1,6 +1,10 @@
 import numpy as np
 import warnings
 from scipy.interpolate import CubicSpline
+from scipy.ndimage import gaussian_filter
+from hexrd.wppf.peakfunctions import (
+    _split_unit_gaussian as sp_gauss)
+    #_split_unit_pv as sp_pv)
 
 class Amorphous:
     '''
@@ -46,14 +50,21 @@ class Amorphous:
         much (if any) gaussian smoothing to apply to the lineout
     '''
     def __init__(self,
-                 model_type='split_pv',
+                 tth_list,
+                 model_type='split_gaussian',
                  model_data=None,
-                 scale=None,
-                 shift=None,
-                 smoothing=None,):
+                 scale=1.,
+                 shift=0.,
+                 smoothing=0,
+                 center=30.,
+                 fwhm=np.array([5, 5])):
         '''
         Parameters
         ----------
+        tth_list: numpy.ndarray
+            list of two-theta values for which amorphous
+            intensity is computed
+
         model_type: str
             type of model to use for amorphous peak.
             this could be a predefined model such as
@@ -66,35 +77,167 @@ class Amorphous:
             for amporphous peak. this model will be 
             shifted and scaled to minimize the difference
             between observed and calculated intensities
+
+        scale: float
+            scaling factor for the experimentally measured
+            signal
+
+        shift: float
+            shift in two-theta for the experimental signal
+            to match the observations
+
+        smoothing: int
+            width of gaussian kernel smoothing function
+
+        center: float
+            center of split gaussian or pseudo-voight function
+
+        fwhm: numpy.ndarray
+            array of shape [2,] with [fwhm_l, fwhm_r] of the 
+            two halves
         '''
+        self.tth_list = tth_list
 
         self.model_type = model_type
         self.model_data = model_data
 
-        @property
-        def model_type(self):
-            return self._model_type
+        self.scale = scale
         
-        @model_type.setter
-        def model_type(self, mtype):
-            if mtype.lower() in ["split_pv",
-                                 "split_gaussian",
-                                 "experimental"]:
-                self._model_type = mtype
-            else:
-                msg = (f'{mtype} is an unknown model type')
-                raise ValueError(msg)
+        self._shift = shift
+        self._smoothing = smoothing
 
+        self._center = center
+        self._fwhm = fwhm
+
+    @property
+    def model_type(self):
+        return self._model_type
+    
+    @model_type.setter
+    def model_type(self, mtype):
+        if mtype.lower() in ["split_pv",
+                             "split_gaussian",
+                             "experimental"]:
+            self._model_type = mtype
+        else:
+            msg = (f'{mtype} is an unknown model type')
+            raise ValueError(msg)
+
+    @property
+    def tth_list(self):
+        return self._tth_list
+
+    @tth_list.setter
+    def tth_list(self, val):
+        if isinstance(val, np.ndarray):
+            self._tth_list = val
+        elif isinstance(val, (list, tuple)):
+            self._tth_list = np.array(val)
+        else:
+            msg = f'{type(val)} not supported for tth_list'
+            raise ValueError(msg)
+
+    @property
+    def model_data(self):
+        return self._model_data
+
+    @model_data.setter
+    def model_data(self, data):
         if self.model_type.lower() == "experimental":
-            if model_data is not None:
-                self.model_data = model_data
+            if data is not None:
+                self._model_data = data
             else:
                 msg = (f'experimental model is being used. '
-                       f'Please supply the data array')
+                       f'please supply the data array')
                 raise ValueError(msg)
         else:
-            if model_data is not None:
+            if data is not None:
                 msg = (f'model data supplied will be ignored'
-                       f'for model type {mtype}.')
+                       f'for model type {self.model_type}')
                 warnings.warn(msg)
 
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(self, val):
+        self._scale = val
+
+    @property
+    def shift(self):
+        if self.model_type == "experimental":
+            return self._shift
+        return None
+
+    @shift.setter
+    def shift(self, val):
+        if self.model_type == "experimental":
+            self._shift = val
+        else:
+            msg = (f'can not set shift for '
+                   f'model_type {self.model_type}')
+            warnings.warn(msg)
+
+    @property
+    def smoothing(self):
+        if self.model_type == "experimental":
+            return self._smoothing
+        return None
+
+    @smoothing.setter
+    def smoothing(self, val):
+        if self.model_type == "experimental":
+            self._smoothing = val
+        else:
+            msg = (f'can not set smoothing for '
+                   f'model_type {self.model_type}')
+            warnings.warn(msg)
+
+    @property
+    def center(self):
+        if self.model_type in ["split_gaussian",
+                               "split_pv"]:
+            return self._center
+
+        return None
+
+    @center.setter
+    def center(self, val):
+        if self.model_type in ["split_gaussian",
+                               "split_pv"]:
+            self._center = val
+        else:
+            msg = (f'can not set center for '
+                   f'model_type {self.model_type}')
+            warnings.warn(msg)
+
+    @property
+    def fwhm(self):
+        if self.model_type in ["split_gaussian",
+                               "split_pv"]:
+            return self._fwhm
+
+        return None
+
+    @fwhm.setter
+    def fwhm(self, val):
+        if self.model_type in ["split_gaussian",
+                               "split_pv"]:
+            self._fwhm = val
+        else:
+            msg = (f'can not set fwhm for '
+                   f'model_type {self.model_type}')
+            warnings.warn(msg)
+
+    @property
+    def amorphous_lineout(self):
+        if self.model_type == "experimental":
+            return self.scale*gaussian_filter(
+                                   self.model_data,
+                                   self.smoothing
+                                   )
+        elif self.model_type == "split_gaussian":
+            p = np.hstack((self.center,
+                           self.fwhm))
+            return self.scale*sp_gauss(p, self.tth_list)
