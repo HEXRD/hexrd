7490627789d06adaaf3875da6c758f561c9d002b
Saransh Singh <saransh13@gmail.com>
2025-11-21 13:22:22 -0800

    phase transformation variants

---
+++ hexrd/singlextal/phasetransformation/variants.py
@@ -0,0 +1,178 @@
+import numpy as np
+from hexrd.rotations import misorientation
+from hexrd.material import Material
+from matplotlib import pyplot as plt
+from hexrd.valunits import _angstrom, _kev
+import mplstereonet
+from numba import njit
+
+"""
+define some helper functions
+"""
+
+"""
+    remove all hkl, -hkl pairs from list of symmetrically
+equivalent hkls"""
+
+
+def removeinversion(ksym):
+    klist = []
+    for i in range(ksym.shape[0]):
+        k = ksym[i, :]
+        kk = list(k)
+        nkk = list(-k)
+        if klist == []:
+            if np.sum(k) > np.sum(-k):
+                klist.append(kk)
+            else:
+                klist.append(nkk)
+
+        else:
+            if (kk in klist) or (nkk in klist):
+                pass
+            else:
+                klist.append(kk)
+    klist = np.array(klist)
+    return klist
+
+
+"""
+    get expected number of phase transformation variants
+    from group theoretic calculations
+"""
+
+
+def expected_num_variants(mat1, mat2, R1, R2):
+    """
+    calculate expected number of orientational
+    variants using the formula given in
+    C. Cayron, J. Appl. Cryst. (2007). 40, 1179–1182
+    page 2
+    N_alpha = |G_beta|/|H_beta|
+    """
+    T = np.dot(R1, R2.T)
+    sym1 = mat1.unitcell.SYM_PG_c
+    sym2 = mat2.unitcell.SYM_PG_c
+    ctr = 0
+    for s2 in sym2:
+        ss = np.dot(T, np.dot(s2, T.T))
+        for s1 in sym1:
+            diff = np.sum(np.abs(ss - s1))
+            if diff < 1e-6:
+                ctr += 1
+    return int(len(sym1) / ctr)
+
+
+"""
+    get rotation matrix
+"""
+
+
+def get_rmats(p1, d1, mat, toprint=False):
+    sym = mat.unitcell.SYM_PG_c
+    z = p1
+    x = d1
+    y = np.cross(z, x)
+    return np.vstack((x, y, z)).T
+
+
+"""
+    check if rotation matrix generated is a new one
+"""
+
+
+def isnew(mat, rmat, sym):
+    res = True
+    for r in rmat:
+        for s in sym:
+            rr = np.dot(s, r)
+            diff = np.sum(np.abs(rr - mat))
+            if diff < 1e-6:
+                res = False
+                break
+    return res
+
+
+def prepare_data(mat1, mat2, parallel_planes, parallel_directions):
+    """
+    prepare the planes and directions by:
+        1. converting to cartesian space
+        2. normalizing magnitude"""
+    p1, p2 = parallel_planes
+    d1, d2 = parallel_directions
+
+    p1 = mat1.unitcell.TransSpace(p1, "r", "c")
+    p1 = mat1.unitcell.NormVec(p1, "c")
+    d1 = mat1.unitcell.TransSpace(d1, "d", "c")
+    d1 = mat1.unitcell.NormVec(d1, "c")
+
+    p2 = mat2.unitcell.TransSpace(p2, "r", "c")
+    p2 = mat2.unitcell.NormVec(p2, "c")
+    d2 = mat2.unitcell.TransSpace(d2, "d", "c")
+    d2 = mat2.unitcell.NormVec(d2, "c")
+
+    return (p1, p2), (d1, d2)
+
+
+# main function
+# get the variants
+def getOR(R1, R2, mat1, mat2):
+    """
+    R1 ---> mat1
+    R2 ---> mat2
+    """
+    rmat = []
+    sym1 = mat1.unitcell.SYM_PG_c
+    sym2 = mat2.unitcell.SYM_PG_c
+    for sa in sym1:
+        ma = np.dot(sa, R1)
+        # for sb in sym2:
+        #     mb = np.dot(sb, R2)
+        mb = R2
+        m = np.dot(mb, ma.T)
+        if isnew(m, rmat, sym2):
+            rmat.append(m)
+
+    rmat_t = [r.T for r in rmat]
+    return rmat_t
+
+
+def getPhaseTransformationVariants(
+    mat1, mat2, parallel_planes, parallel_directions, plot=False
+):
+    """
+    main function to get the phase transformation variants between two materials
+    give a set of parallel planes and parallel directions
+
+    Parameters
+    ----------
+    mat1 : hexrd.material.Material
+        Material class for parent phase
+    mat2 : hexrd.material.Material
+        Material class for child phase
+    parallel_planes : list/tuple
+        list of numpy arrays with parallel planes, length=2
+    parallel_planes : list/tuple
+        list of numpy arrays with parallel directions, length=2
+    plot : boolean
+        plot the data in stereographic projection if true
+
+
+    """
+    (p1, p2), (d1, d2) = prepare_data(
+        mat1, mat2, parallel_planes, parallel_directions
+    )
+
+    R1 = get_rmats(p1, d1, mat1)
+    R2 = get_rmats(p2, d2, mat2)
+
+    rmat = getOR(R1, R2, mat1, mat2)
+
+    num_var = expected_num_variants(mat1, mat2, R1, R2)
+
+    print("Expected # of orientational variants = ", num_var)
+    print(
+        "number of orientation variants in phase transition = ",
+        len(rmat),
+        "\n",
+    )
