ef68e8e8d84ae31255e2d9cd685e16588c4bb4ca
Zack Singer <zack.singer@verdantevolution.com>
2026-02-04 10:22:38 -0500

    Resolve importlib typing issues

--- hexrd/core/utils/panel_buffer.py
+++ hexrd/core/utils/panel_buffer.py
@@ -76,20 +76,18 @@ def panel_buffer_from_str(name: str, panel: Detector) -> np.ndarray:
     return roi_buffer
 
 
-# TODO: Broken function - dir_path.glob is not defined
 def valid_panel_buffer_names() -> list[str]:
     dir_path = importlib.resources.files(hexrd.core.resources.panel_buffers)
-    return [path.stem for path in dir_path.glob('*.npz')]
+    return [importlib.resources.as_file(file_) for file_ in dir_path.iterdir() if file_.name.endswith('.npz')]
 
 
 # Cache this so we only read from disk once
-# TODO: Broken function - path.exists is not defined
 @cache
 def _load_panel_buffer_from_file(name: str) -> np.ndarray:
     path = importlib.resources.files(hexrd.core.resources.panel_buffers).joinpath(
         f'{name}.npz'
     )
-    if not path.exists():
+    if not path.is_file():
         raise NotImplementedError(f'Unknown panel buffer name: {name}')
 
     npz = np.load(str(path))
