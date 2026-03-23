ac91116c04bb765a0b88f18a7fbf07163e114442
Zack Singer <zack.singer@verdantevolution.com>
2026-03-23 11:02:38 -0400

    Remove fit_grains_check.py

--- tests/test_fit-grains.py
+++ tests/test_fit-grains.py
@@ -7,12 +7,10 @@ import numpy as np
 import pytest
 import coloredlogs
 
-
+from hexrd.core import matrixutil as mutil
+from hexrd.core import rotations as rot
 from hexrd.hedm import config
 from hexrd.hedm.fitgrains import fit_grains
-
-
-from fit_grains_check import compare_grain_fits
 
 
 logger = logging.getLogger(__name__)
@@ -73,6 +71,68 @@ def test_config(single_ge_config_path, single_ge_include_path):
 
     return conf
 
+def compare_grain_fits(
+    fit_grain_params, ref_grain_params, mtol=1.0e-4, ctol=1.0e-3, vtol=1.0e-4
+):
+    """
+    Executes comparison between reference and fit grain parameters for ff-HEDM
+    for the same initial parameters.
+
+    Parameters
+    ----------
+    fit_grain_params : array_like, (n, 12)
+        The fit grain parameters to be tested.
+    ref_grain_params : array_like, (n, 12)
+        The reference grain parameters (see Notes below).
+
+    Returns
+    -------
+    bool
+        True is successful comparison
+
+    Notes
+    -----
+    The fitgrains action currently returns
+        grain_id, completeness, chisq, grain_params.
+    We will have to assume that the grain_ids are in the *same order* as the
+    reference, which can be enforces by running the comparison using the
+    reference orientation list.
+    """
+    fit_grain_params = np.atleast_2d(fit_grain_params)
+    ref_grain_params = np.atleast_2d(ref_grain_params)
+    cresult = False
+    ii = 0
+    for fg, rg in zip(fit_grain_params, ref_grain_params):
+        # test_orientation
+        quats = rot.quatOfExpMap(np.vstack([fg[:3], rg[:3]]).T)
+        ang, mis = rot.misorientation(
+            quats[:, 0].reshape(4, 1), quats[:, 1].reshape(4, 1)
+        )
+        if ang <= mtol:
+            cresult = True
+        else:
+            logger.warning(f"orientations for grain {ii} do not agree.")
+            return cresult
+
+        # test position
+        if np.linalg.norm(fg[3:6] - rg[3:6]) > ctol:
+            logger.warning(f"centroidal coordinates for grain {ii} do not agree.")
+            return False
+
+        # test strain
+        vmat_fit = mutil.symmToVecMV(
+            np.linalg.inv(mutil.vecMVToSymm(fg[6:])), scale=False
+        )
+        vmat_ref = mutil.symmToVecMV(
+            np.linalg.inv(mutil.vecMVToSymm(rg[6:])), scale=False
+        )
+        if np.linalg.norm(vmat_fit - vmat_ref, ord=1) > vtol:
+            logger.warning(f"stretch components for grain {ii} do not agree.")
+            return False
+
+        # index grain id
+        ii += 1
+    return cresult
 
 def test_fit_grains(
     single_ge_include_path,
