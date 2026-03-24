42d3118010b83092b8270b024a67d3a71edabb00
Zack Singer <zack.singer@verdantevolution.com>
2026-03-24 11:40:23 -0400

    Move fitting __init__.py file.

--- hexrd/hedm/fitting/__init__.py
+++ hexrd/hedm/fitting/__init__.py
@@ -25,4 +25,4 @@
 # Boston, MA 02111-1307 USA or visit <http://www.gnu.org/licenses/>.
 # ============================================================
 
-from hexrd.hedm.fitting.grains import fitGrain, objFuncFitGrain, gFlag_ref
+from .grains import fitGrain, objFuncFitGrain, gFlag_ref
