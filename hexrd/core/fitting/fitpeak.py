5f2028cab8d0a96aa42aafba0727391eea5dacf0
Patrick Avery <patrick.avery@kitware.com>
2026-05-13 15:02:48 -0500

    Use Tau default values from `fitpeak.py`
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- hexrd/core/fitting/fitpeak.py
+++ hexrd/core/fitting/fitpeak.py
@@ -57,7 +57,7 @@ minf = -inf
 # dcs param values
 # !!! converted from deg^-1 in Von Dreele's paper
 alpha0, alpha1, beta0, beta1 = np.r_[14.4, 0.0, 3.016, -7.94]
-tau0, tau1, tau2 = np.r_[-0.043, 2.442, -5.49]
+tau0_DFLT, tau1_DFLT, tau2_DFLT = np.r_[-0.043, 2.442, -5.49]
 
 
 def cnst_fit_obj(x, b):
@@ -170,7 +170,7 @@ def estimate_pk_parms_1d(x, f, pktype='pvoigt'):
         p = [A, x0, alpha0, alpha1, beta0, beta1, FWHM, FWHM, bg0, bg1]
     elif pktype == 'pink_beam_heating':
         # A, x0, tau0, tau1, tau2, fwhm_g, fwhm_l
-        p = [A, x0, tau0, tau1, tau2, FWHM, FWHM, bg0, bg1]
+        p = [A, x0, tau0_DFLT, tau1_DFLT, tau2_DFLT, FWHM, FWHM, bg0, bg1]
     else:
         raise RuntimeError("pktype '%s' not understood" % pktype)
 
@@ -540,9 +540,9 @@ def estimate_mpk_parms_1d(
             p0tmp[ii, :] = [
                 amp_guess,
                 pk_pos_0[ii],
-                tau0,
-                tau1,
-                tau2,
+                tau0_DFLT,
+                tau1_DFLT,
+                tau2_DFLT,
                 fwhm_guess[ii],
                 fwhm_guess[ii],
             ]
